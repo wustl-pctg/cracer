d89f08dcceb996aa94b04b117bf52d5e6ada83d3
Brendan Sheridan <bss45@georgetown.edu>
2013-04-10 14:48:12 +0000

    added defaults.h to testbed for keeping test case default parameters

---
+++ split/testbed/defaults.h
@@ -0,0 +1,8 @@
+#ifndef _DEFAULTS_H
+#define _DEFAULTS_H
+
+#define DUMMY_WAIT 100000 //loop iterations to wait (to simulate operation time)
+#define DUMMY_ITERS 10000 //number of times to execute operation
+#define BASE_SIZE 100
+
+#endif
