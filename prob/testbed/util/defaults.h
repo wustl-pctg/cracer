d60fd1d23ba017b06406685e532456261d201777
Robert Utterback <robertu13@gmail.com>
2013-06-28 19:44:35 +0000

    Added a defaults.h file to keep default constant values.

---
+++ prob/testbed/util/defaults.h
@@ -0,0 +1,10 @@
+#ifndef _DEFAULTS_H
+#define _DEFAULTS_H
+
+#define PARFOR_THRESHOLD 128
+
+#define DUMMY_WAIT 512 ///< Loop iteration to wait (to simulate operation time)
+#define DUMMY_ITERS 131072 ///< Number of times to execute operation
+#define BASE_SIZE 128
+
+#endif
