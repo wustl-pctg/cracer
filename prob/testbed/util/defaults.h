56f93d850de6ce8fd2b3ad222b067f32b4654195
Robert Utterback <robertu13@gmail.com>
2013-07-19 20:57:39 +0000

    Makefile updates. The essential testing setup is basically reading, though I haven't written anything to create plots.

--- prob/testbed/util/defaults.h
+++ prob/testbed/util/defaults.h
@@ -5,6 +5,10 @@
 #define PARFOR_THRESHOLD 128
 #endif
 
+#ifndef PARFORPAR_THRESHOLD
+#define PARFORPAR_THRESHOLD 32
+#endif
+
 #define DUMMY_WAIT 512 ///< Loop iteration to wait (to simulate operation time)
 #define DUMMY_ITERS 131072 ///< Number of times to execute operation
 #define BASE_SIZE 128
