c88d1014998dc2d2653ff87d2ade37dcaf64b977
Robert Utterback <robertu13@gmail.com>
2013-08-06 22:06:42 +0000

    Fixes to graph.R

--- graph.R
+++ graph.R
@@ -4,14 +4,14 @@
 
 args <- commandArgs(trailingOnly = TRUE)
 if (is.na(args[1])) {
-	 operations <- 90000
+	 operations <- 100000
 } else if (suppressWarnings(is.na(as.numeric(args[1])))) {
 	stop("Invalid argument")
 } else {
 	operations <- as.numeric(args[1])
 }
 
-data <- read.csv(file="param.log.csv", header=TRUE, sep=",")
+data <- read.csv(file="param.08051546.log", header=TRUE, sep=",")
 subset <- data[data$OPS == operations,]
 pdf(file="test.pdf")
 
@@ -20,7 +20,7 @@ y_range <- range(0, subset$BATCH, subset$LOCKS)
 plot(subset$WORKTIME, subset$BATCH, type="o", col="blue", ylim=y_range,
 									axes = FALSE, ann = FALSE)
 lines(subset$WORKTIME, subset$LOCKS, type="o", pch=22, lty=2, col="red")
-axis(1, at=subset$WORK, lab=subset$WORK)
+axis(1, at=subset$WORKTIME, lab=subset$WORKTIME)
 axis(2, las=1, at=1000*0:y_range[2])
 
 title(main=paste("Execution Time vs. Work with ", operations, " Operations"),
