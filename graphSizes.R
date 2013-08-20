e6b8c80fbbd1cc523f2767f55a7e7f149abf0557
Robert Utterback <robertu13@gmail.com>
2013-08-20 22:09:23 +0000

    graphSizes.R can now plot a histogram of batch size data

--- graphSizes.R
+++ graphSizes.R
@@ -1,41 +1,25 @@
 #!/usr/bin/env Rscript
 
+# Filled Kernel Density Plot
+## d <- density(univariateData)
+## plot(d, main="Kernel Density of blah balh")
+## polydon(d, col="red", border="blue")
+
+#inverse.rle(list(lengths=COUNTS, values=HEADER))
+
 args <- commandArgs(trailingOnly = TRUE)
 if (is.na(args[1])) {
-	 logFile <- "param.log"
+	 logFile <- "sizes.log"
 } else {
 	logFile <- args[1]
 }
 if (is.na(args[2])) {
-	 outFile <- "graph.pdf"
+	 outFile <- "sizes.pdf"
 } else {
 	outFile <- args[2]
 }
-if (is.na(args[3])) {
-	 operations <- 100000
-} else {
-	operations <- as.numeric(args[3])
-}
 
-data <- read.csv(file=logFile, header=TRUE, sep=",")
-subset <- data[data$OPS == operations,]
-pdf(file=outFile)
+data <- read.csv(file=logFile, header=TRUE, sep=",", check.names=FALSE)
+h <- inverse.rle(list(lengths=data[1,], values=as.numeric(colnames(data))))
 
-y_range <- range(0, subset$BATCH, subset$LOCKS)
-
-plot(subset$WORKTIME, subset$BATCH, type="o", col="blue", ylim=y_range,
-									axes = FALSE, ann = FALSE)
-lines(subset$WORKTIME, subset$LOCKS, type="o", pch=22, lty=2, col="red")
-axis(1, at=subset$WORKTIME, lab=subset$WORKTIME)
-axis(2, las=1, at=1000*0:y_range[2])
-
-title(main=paste("Execution Time vs. Work with ", operations, " Operations"),
-														col.main="red", font.main=4)
-title(xlab="Work", col.lab=rgb(0,0.5,0))
-title(ylab="Time", col.lab=rgb(0,0.5,0))
-legend(1, y_range[2], c("batch", "locks"), cex=0.8,
-					col=c("blue","red"), pch=21:22, lty=1:2);
-
-
-#axis(1, at=subset$WORK, lab=subset$WORK)
 dev.off()
