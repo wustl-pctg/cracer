93aa6f7ba2a367ef78963b3675ee5584f673be19
Robert Utterback <robertu13@gmail.com>
2013-08-26 04:42:47 +0000

    graphSizes.R makes a better graph now.

--- graphSizes.R
+++ graphSizes.R
@@ -21,5 +21,18 @@ if (is.na(args[2])) {
 
 data <- read.csv(file=logFile, header=TRUE, sep=",", check.names=FALSE)
 h <- inverse.rle(list(lengths=data[1,], values=as.numeric(colnames(data))))
+f <- factor(h)
 
+pdf(file=outFile)
+plot(f, col = "darkslategray4", border = "seashell3", space=0)
+title(main = paste("Histogram of Batch Sizes (",length(h)," batches)", sep=""),
+      col.main="red", font.main=4)
+title(xlab="Batch Sizes", col.lab=rgb(0,0.5,0))
+title(ylab="Frequency", col.lab=rgb(0,0.5,0))
+box()
+
+## Other plotting options:
+## plot(density(h))
+## hist(h)
+    
 dev.off()
