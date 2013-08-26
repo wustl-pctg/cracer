ddca4eec9164627f00d18a103f3af085dda665fa
Robert Utterback <robertu13@gmail.com>
2013-08-26 04:42:10 +0000

    benchSizes.sh now works correctly.

--- benchSizes.sh
+++ benchSizes.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 NPROC=16
-WORK=0.1 #1.0
-OPS=1  #100000
+WORK=1.0
+OPS=100000
 ITER=5
 
 DATE=$(date +%m%d%H%M)
@@ -14,11 +14,19 @@ if [ $? -ne 0 ]; then
 fi
 cd ../..
 
+HEADER=
+for ((i = 1; i <= NPROC; i += 1))
+do
+		HEADER="${HEADER}$i,"
+done
+HEADER=$(echo $HEADER | sed s'/,$//')
+
 for ((BTEST = 0; BTEST <= 3; BTEST += 1))
 do
-		FILE="my.sizes${BTEST}.${DATE}.log";
-		echo "WORKTIME,REALTIME,WORK,OPS,NPROC,ITER,LOCKS,BATCH,RATIO" >> $FILE;
-		prob/testbed/param --nproc $NPROC --btest $BTEST --stats -1 -w $WORK -o $OP -i $ITER >> $FILE 2>> $FILE
-		cat $FILE | grep "Batch sizes" | tr -d 'Batch sizes:' | tr -d ' ' | sed s'/,$//' > temp
+		FILE="sizes${BTEST}.${DATE}.log";
+		prob/testbed/param --nproc $NPROC --btest $BTEST --stats -1 -w $WORK -o $OP -i $ITER 2>> $FILE
+
+		echo $HEADER > temp
+		cat $FILE | grep "Batch sizes" | tr -d 'Batch sizes:' | tr -d ' ' | sed s'/,$//' >> temp
 		mv temp $FILE
 done
