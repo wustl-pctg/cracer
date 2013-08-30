14dc4e290cae1b958cda6551699846f0c2027ffa
Robert Utterback <robertu13@gmail.com>
2013-08-30 14:59:18 +0000

    Changes to graphParam.r

--- graphParam.R
+++ graphParam.R
@@ -1,4 +1,5 @@
 #!/usr/bin/env Rscript
+require(gplots)
 
 args <- commandArgs(trailingOnly = TRUE)
 if (is.na(args[1])) {
@@ -26,16 +27,17 @@ y_range <- range(0, subset$BATCH, subset$LOCKS)
 plot(subset$WORKTIME, subset$BATCH, type="o", col="blue", ylim=y_range,
 									axes = FALSE, ann = FALSE)
 lines(subset$WORKTIME, subset$LOCKS, type="o", pch=22, lty=2, col="red")
+lab <- seq(0, y_range[2], ceiling(signif(y_range[2],digits=2))/10)
 axis(1, at=subset$WORKTIME, lab=subset$WORKTIME)
-axis(2, las=1, at=1000*0:y_range[2])
+axis(2, at=lab, lab=lab/1000)
+title(main=paste("Execution Time vs. Work with ", formatC(operations, format="d"), " Operations"),
+      col.main="red", font.main=4)
+title(xlab="Work (ms)", col.lab=rgb(0,0.5,0))
+title(ylab="Time (s)", col.lab=rgb(0,0.5,0))
 
-title(main=paste("Execution Time vs. Work with ", operations, " Operations"),
-														col.main="red", font.main=4)
-title(xlab="Work", col.lab=rgb(0,0.5,0))
-title(ylab="Time", col.lab=rgb(0,0.5,0))
-legend(1, y_range[2], c("batch", "locks"), cex=0.8,
-					col=c("blue","red"), pch=21:22, lty=1:2);
+## legend("bottomright", c("batch", "locks"), cex=0.8,
+##        col=c("blue","red"), pch=21:22, lty=1:2)
+smartlegend(x="right", y="bottom", c("batch", "locks"), cex=0.8,
+            col=c("blue","red"), pch=21:22, lty=1:2)
 
-
-#axis(1, at=subset$WORK, lab=subset$WORK)
 dev.off()
