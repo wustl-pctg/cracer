fb7ab15f399be1a8fcf9d5d32c57b081808b471d
Kefu Lu <kefulu@wustl.edu>
2013-09-30 17:00:28 +0000

    added better scripts, just need to run and plot now

--- benchThroughput.py
+++ benchThroughput.py
@@ -14,19 +14,20 @@ nproc = range(16,17,1)
 probdir = 'prob'
 fcdir = 'flat_combining'
 fctime = 10 # seconds to run the flat combining benchmark
-batchops=200000
+batchops=2000000
 fcinitialsize=20000
 dedicated=0
 
 current = datetime.datetime.now();
-filename = "stack{0}-{1}-{2}-{3}.log".format(current.month,current.day,current.hour,current.minute)
+filename = "stackBench{0}-{1}-{2}-{3}.log".format(current.month,current.day,current.hour,current.minute)
 outFile = open(filename,"w");
-outFile.write("PROCS,BATCH,FC\n");
+outFile.write("PROCS,BATCH,BATCH_NO_STEAL,FC_50_50,FC_80_20\n");
 for p in nproc:
   for i in iterations:
-    throughput=[0,0]
+    throughput=[0,0,0,0]
     batchcmd = probdir + '/testbed/' + test
     batch_args = ' --nproc ' + str(p) + ' -o ' + str(batchops) 
+    batch_no_steal_args = ' --nproc ' + str(p) +' --batchprob 0 --dsprob 0 ' + ' -o ' + str(batchops) 
     
     # Arguments:
     # alg1_name alg1_num alg2_name alg2_num alg3_name alg3_num
@@ -35,7 +36,9 @@ for p in nproc:
     # read_write_delay
     fccmd =  fcdir + '/test_intel64 '
     fc_50_50_args = 'fcstack 1 non 0 non 0 non 0 1 '+ str(p) + ' 50 50 0.0 '
+    fc_80_20_args = 'fcstack 1 non 0 non 0 non 0 1 '+ str(p) + ' 80 20 0.0 '
     fc_50_50_args += str(fcinitialsize) + ' ' + str(fctime) + ' ' + str(dedicated) + ' 0 0'
+    fc_80_20_args += str(fcinitialsize) + ' ' + str(fctime) + ' ' + str(dedicated) + ' 0 0'
     
     
     #First run batch
@@ -46,15 +49,28 @@ for p in nproc:
     tp = string.split(output)
     throughput[0]=batchops/float(tp[0])
 
+    process = subprocess.Popen(batchcmd+batch_no_steal_args, shell=True,
+      stdout=subprocess.PIPE,
+      stderr=subprocess.STDOUT)
+    output = process.communicate()[0]
+    tp = string.split(output)
+    throughput[1] = batchops/float(tp[0])
+
     #now run FC
     #print fccmd,fcargs
     process = subprocess.Popen(fccmd + fc_50_50_args, shell=True,
                                stdout=subprocess.PIPE,
                                stderr=subprocess.PIPE)
     output = process.communicate()[0]
-    throughput[1]=(string.split(output))[0]
+    throughput[2]=(string.split(output))[0]
  
+    process = subprocess.Popen(fccmd + fc_80_20_args, shell=True,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    output = process.communicate()[0]
+    throughput[3]=(string.split(output))[0]
+
     #then print output
-#    print throughput
-    outFile.write("{0},{1},{2}\n".format(str(p),math.floor(throughput[0]),throughput[1]))
-    print "batch,fc",throughput[0],throughput[1]
+    #print throughput
+    outFile.write("{0},{1},{2},{3},{4}\n".format(str(p),math.floor(throughput[0]),math.floor(throughput[1]),
+                                                 throughput[2],throughput[3]))
