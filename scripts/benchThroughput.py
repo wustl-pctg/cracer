594ed156498ff14126dd3148076d136295276c8c
Kefu Lu <kefulu@wustl.edu>
2013-10-09 19:03:05 +0000

    resolved conflict for throughput

--- scripts/benchThroughput.py
+++ scripts/benchThroughput.py
@@ -11,20 +11,22 @@ test='stackBatch_test'
 iterations = range(1)
 #nproc = range(1, 17, 1)
 nproc = range(16,17,1)
-probdir = 'prob'
-fcdir = 'flat_combining'
-fctime = 10 # seconds to run the flat combining benchmark
-batchops=2000000
+probdir = '../prob'
+fcdir = '../flat_combining'
+fctime = 5 # seconds to run the flat combining benchmark
+batchops=20000
 fcinitialsize=20000
 dedicated=0
+logdir = '../logs/'
+graphdir = '../graphs/'
 
 current = datetime.datetime.now();
 filename = "stackBench{0}-{1}-{2}-{3}.log".format(current.month,current.day,current.hour,current.minute)
-outFile = open(filename,"w");
-outFile.write("PROCS,BATCH,BATCH_NO_STEAL,FC_50_50,FC_80_20\n");
+outFile = open(logdir+filename,"w");
+outFile.write("PROCS,B50_50,B50_50_NS,FC_50_50,FC_80_20\n");
 for p in nproc:
   for i in iterations:
-    throughput=[0,0,0,0]
+    throughput=[str(p)]
     batchcmd = probdir + '/testbed/' + test
     batch_args = ' --nproc ' + str(p) + ' -o ' + str(batchops) 
     batch_no_steal_args = ' --nproc ' + str(p) +' --batchprob 0 --dsprob 0 ' + ' -o ' + str(batchops) 
@@ -36,10 +38,12 @@ for p in nproc:
     # read_write_delay
     fccmd =  fcdir + '/test_intel64 '
     fc_50_50_args = 'fcstack 1 non 0 non 0 non 0 1 '+ str(p) + ' 50 50 0.0 '
-    fc_80_20_args = 'fcstack 1 non 0 non 0 non 0 1 '+ str(p) + ' 80 20 0.0 '
     fc_50_50_args += str(fcinitialsize) + ' ' + str(fctime) + ' ' + str(dedicated) + ' 0 0'
+    fc_80_20_args = 'fcstack 1 non 0 non 0 non 0 1 '+ str(p) + ' 80 20 0.0 '
     fc_80_20_args += str(fcinitialsize) + ' ' + str(fctime) + ' ' + str(dedicated) + ' 0 0'
-    
+    fc_90_10_args = 'fcstack 1 non 0 non 0 non 0 1 '+ str(p) + ' 90 10 0.0 '
+    fc_90_10_args += str(fcinitialsize) + ' ' + str(fctime) + ' ' + str(dedicated) + ' 0 0'
+
     
     #First run batch
     process = subprocess.Popen(batchcmd+batch_args, shell=True,
@@ -47,30 +51,49 @@ for p in nproc:
       stderr=subprocess.STDOUT)
     output = process.communicate()[0]
     tp = string.split(output)
-    throughput[0]=batchops/float(tp[0])
+    throughput.append(math.floor(batchops/float(tp[0])))
 
     process = subprocess.Popen(batchcmd+batch_no_steal_args, shell=True,
       stdout=subprocess.PIPE,
       stderr=subprocess.STDOUT)
     output = process.communicate()[0]
     tp = string.split(output)
-    throughput[1] = batchops/float(tp[0])
+    throughput.append(math.floor(batchops/float(tp[0])))
 
-    #now run FC
-    #print fccmd,fcargs
+    #now run FC tests
+    #50_50
     process = subprocess.Popen(fccmd + fc_50_50_args, shell=True,
                                stdout=subprocess.PIPE,
                                stderr=subprocess.PIPE)
     output = process.communicate()[0]
-    throughput[2]=(string.split(output))[0]
- 
+    throughput.append((string.split(output))[0])
+    #80_20
     process = subprocess.Popen(fccmd + fc_80_20_args, shell=True,
                                stdout=subprocess.PIPE,
                                stderr=subprocess.PIPE)
     output = process.communicate()[0]
-    throughput[3]=(string.split(output))[0]
+    throughput.append((string.split(output))[0])
+    #90_10
+    process = subprocess.Popen(fccmd + fc_90_10_args, shell=True,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    output = process.communicate()[0]
+    throughput.append((string.split(output))[0])
+    
 
-    #then print output
-    #print throughput
-    outFile.write("{0},{1},{2},{3},{4}\n".format(str(p),math.floor(throughput[0]),math.floor(throughput[1]),
-                                                 throughput[2],throughput[3]))
+    
+    output = ",".join(str(item) for item in throughput)
+    outFile.write(output);
+    outFile.write("\n");
+    
+    #graph the output
+    plotscript="plot.gnu"
+    arguments="\"filename='{0}';cols={1};outfile='{2}'".format(logdir+filename,len(throughput),graphdir+filename);
+    graphcmd = "gnuplots -e {0} {1}".format(arguments,plotscript);
+    print graphcmd
+    print arguments
+    print plotscript
+    process = subprocess.Popen(fccmd + fc_90_10_args, shell=True,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    output = process.communicate()[0]
