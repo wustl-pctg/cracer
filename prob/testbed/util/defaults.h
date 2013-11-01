58c36e23c7a199a0baa6818ca0205db6ee665b8b
Robert Utterback <robertu13@gmail.com>
2013-11-01 20:04:12 +0000

    Minor code cleanup for util files.

--- prob/testbed/util/defaults.h
+++ prob/testbed/util/defaults.h
@@ -1,19 +1,22 @@
 #ifndef _DEFAULTS_H
 #define _DEFAULTS_H
 
+// Below this, parfor's should execute sequentially
 #ifndef PARFOR_THRESHOLD
-#define PARFOR_THRESHOLD 2
+#define PARFOR_THRESHOLD 8
 #endif
 
+// Below this, parforpar's should execute sequentially
 #ifndef PARFORPAR_THRESHOLD
-#define PARFORPAR_THRESHOLD 2
+#define PARFORPAR_THRESHOLD 8
 #endif
 
 #ifndef BATCH_THRESHOLD
-#define BATCH_THRESHOLD USE_PARAMETER2(active_size)
+//#define BATCH_THRESHOLD USE_PARAMETER2(active_size)
+#define BATCH_THRESHOLD 2 ///< Below this, batches should execute sequentially
 #endif
 
-#define DUMMY_WAIT 512 ///< Loop iteration to wait (to simulate operation time)
-#define DUMMY_ITERS 131072 ///< Number of times to execute operation
-#define BASE_SIZE 128
+/* #define DUMMY_WAIT 512 ///< Loop iteration to wait (to simulate operation time) */
+/* #define DUMMY_ITERS 131072 ///< Number of times to execute operation */
+/* #define BASE_SIZE 128 */
 #endif
