14a74f231f10ad40c880378cd9c9c482d737afe3
Alex Jones <atjalex@gmail.com>
2014-06-09 17:04:49 +0000

    removed unnecessary h

--- toTestSA/runtime/order-maintain.c
+++ toTestSA/runtime/order-maintain.c
@@ -22,11 +22,33 @@
 #include <cilk.h>
 #include <cilk-cilk2c-pre.h>
 #include <cilk-cilk2c.h>
-#include <order-maintain.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 
+/*
+ * Currently implemented as a linked list node
+ */
+typedef struct OMNode_s{
+
+	struct OMNode_s *next;
+
+} OMNode;
+/*
+ * Abstract Order maintenance DS
+ */
+typedef struct linked_list_s {
+
+	OMNode * head, *tail;
+	int size;
+
+} OM_DS;
+
+
+typedef struct insert_op_s {
+ OM_DS * ds; //data struct to operate on
+ OMNode * x, *y;//insert node y after x
+} InsertRecord;
 
 
 
