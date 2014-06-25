ab6a4f8e090649b2fbe31130d3643f006b70dcf5
Alex Jones <atjalex@gmail.com>
2014-06-25 18:27:17 +0000

    asd

---
+++ toTestSA/runtime/OM_DS.h
@@ -0,0 +1,40 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  OM_DS.h
+ *
+ *    Description:  Contains the data structures for order maintenance data structure
+ *
+ *        Version:  1.0
+ *        Created:  06/23/2014 03:13:03 PM
+ *       Revision:  Contains linked list only
+ *       Compiler:  gcc
+ *
+ *         Author:  Alex Jones (aj), atjalex@gmail.com
+ *        Company:  
+ *
+ * =====================================================================================
+ */
+
+#ifndef _OM_DS_H
+#define _OM_DS_H
+
+typedef struct OM_Node_s{
+	struct OM_Node_s *next_english, *next_hebrew;
+	int id;
+
+} OM_Node;
+
+typedef struct OM_DS_s {
+	OM_Node *head,*tail;
+	int size;
+
+} OM_DS;
+
+typedef struct InsertRecord_s{
+	OM_DS *ds;
+	OM_Node *x;
+	OM_Node *y;
+
+} InsertRecord;
+#endif
