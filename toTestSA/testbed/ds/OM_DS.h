829ab969e2327121644e29ccd3cbc336795546cc
Alex Jones <atjalex@gmail.com>
2014-07-02 20:38:36 +0000

    added tags and next to OM_DS

--- toTestSA/testbed/ds/OM_DS.h
+++ toTestSA/testbed/ds/OM_DS.h
@@ -13,6 +13,12 @@ typedef struct OM_DS_s {
 	OM_Node *head,*tail;
 	int size;
 	int Reorder_flag;	
+	struct OM_DS_s *next_e, *next_h;
+	struct OM_DS_s *prev_e, *prev_h;
+	int id;
+
+	unsigned int tag_e, tag_h;
+
 
 } OM_DS;
 
