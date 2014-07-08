39bf395c888490ee8089fac62dd2819874a137df
Alex Jones <atjalex@gmail.com>
2014-07-08 18:53:09 +0000

    here ya go icecream face

---
+++ toTestSA/testbed/OM_DS_mixed_test.c
@@ -0,0 +1,74 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  OM_DS_mixed_test.c
+ *
+ *    Description:  Tests top and bottom lists are working correctly
+ *
+ *        Version:  1.0
+ *        Created:  07/07/2014 03:26:08 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Alex Jones (aj), atjalex@gmail.com
+ *        Company:  
+ *
+ * =====================================================================================
+ */
+
+#include	<stdlib.h>
+#include	<stdio.h>
+#include	"OM_DS.h"
+#include	"OM_DS_bender.h"
+#include	"OM_sublist.h"
+
+int main ( int argc, char *argv[] )
+{
+	int num_nodes = atoi(argv[1]);
+	int i = 0, j;
+	OM_Node ** nodeArray = malloc(num_nodes * sizeof(OM_Node *));
+	OM_DS * firstOM_DS =	(OM_DS *)OM_DS_init();
+	firstOM_DS->id = 1;
+	Top_List * list = init_top_list(); 
+
+	for (;i < num_nodes; i++)
+	{
+		nodeArray[i]= malloc(sizeof(OM_Node));
+		nodeArray[i]->id = i;
+		
+	}
+	
+	append_first_list(list, firstOM_DS, ENGLISH_ID);
+
+	OM_DS_add_first_node(firstOM_DS, nodeArray[0]);
+
+
+	i = 1;
+	for (; i < num_nodes; i++)
+	{
+		 if (OM_DS_insert(nodeArray[i - 1], nodeArray[i], ENGLISH_ID))
+		 {
+		 	printf("TEST: splitting and adding to top at test index %i\n", i);
+
+		 	Split_and_add_to_top(list, nodeArray[i]->ds);
+			Rebalance_bottom_lists(list);
+		 }
+
+		if (OM_DS_insert(nodeArray[i - 1], nodeArray[i], HEBREW_ID))
+		{
+			printf("TEST: splitting and adding to top at test index %i\n", i);
+
+			Split_and_add_to_top(list, nodeArray[i]->ds);
+			Rebalance_bottom_lists(list);
+		
+		}
+		printf("Check for correctness..");
+		check_correctness(list);
+		printf("..done\n");
+	}
+	print_top_list(list);
+	check_correctness(list);
+	return EXIT_SUCCESS;
+}				/* ----------  end of function main  ---------- */
+
+
