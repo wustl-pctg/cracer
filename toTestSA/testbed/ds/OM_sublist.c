08c45b8c5683a6d3395c1b44427130050f8f4b78
Shane Deiley <shanedeiley@wustl.edu>
2014-07-14 17:29:32 +0000

    updated tests

--- toTestSA/testbed/ds/OM_sublist.c
+++ toTestSA/testbed/ds/OM_sublist.c
@@ -92,7 +92,7 @@ void Split_and_add_to_top(Top_List * tlist, Bottom_List * blist) {
 
 	int node_count_e = 1, node_count_h = 1;
 	/// English
-	if (blist->size_e > 3){
+	if (blist->size_e > 32){
 		unsigned long skip_size = (MAX_NUMBER - 1) / (unsigned long)((blist->size_e >> 1) + 2) ;
 		
 
@@ -155,7 +155,7 @@ void Split_and_add_to_top(Top_List * tlist, Bottom_List * blist) {
 		// Update middle node's referenece to next_e
 		middle_node->next_e = blist->tail;
 	}
-	if (blist->size_h > 3 ) {
+	if (blist->size_h > 32 ) {
 		unsigned long skip_size = (MAX_NUMBER - 1) / (unsigned long)((blist->size_h >> 1) + 2) ;
 		
 
@@ -249,7 +249,7 @@ void Rebalance_bottom_lists(Top_List * list) {
 			Split_and_add_to_top(list, current_h);
 		current_h = current_h->next_h;
 	}
-	check_sub_correctness(list);
+	;//DEBUG: check_sub_correctness(list);
 	
 }
 
