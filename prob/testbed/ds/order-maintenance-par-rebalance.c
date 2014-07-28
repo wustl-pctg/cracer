9eb8725922ebc70816ed24b977ccade8c1e738f4
Shane Deiley <shanedeiley@wustl.edu>
2014-07-28 16:36:20 +0000

    tree

--- prob/testbed/ds/order-maintenance-par-rebalance.c
+++ prob/testbed/ds/order-maintenance-par-rebalance.c
@@ -1109,3 +1109,55 @@ void check_sub_correctness (Top_List * list)
 	}
 }
 
+
+int _print_t(Internal_Node * tree, int is_left, int offset, int depth, char s[20][255])
+{
+    char b[20];
+    int width = 5;
+
+    if (!tree) return 0;
+
+    printf("(%i, %p, %p, %p)", tree->base, tree->left, tree->right, tree);
+
+    int left  = _print_t(tree->left,  1, offset,                depth + 1, s);
+    int right = _print_t(tree->right, 0, offset + left + width, depth + 1, s);
+
+    for (int i = 0; i < width; i++)
+        s[2 * depth][offset + left + i] = b[i];
+
+    if (depth && is_left) {
+
+        for (int i = 0; i < width + right; i++)
+            s[2 * depth - 1][offset + left + width/2 + i] = '-';
+
+        s[2 * depth - 1][offset + left + width/2] = '+';
+        s[2 * depth - 1][offset + left + width + right + width/2] = '+';
+
+    } else if (depth && !is_left) {
+
+        for (int i = 0; i < left + width; i++)
+            s[2 * depth - 1][offset - width/2 + i] = '-';
+
+        s[2 * depth - 1][offset + left + width/2] = '+';
+        s[2 * depth - 1][offset - width/2 - 1] = '+';
+    }
+
+    return left + width + right;
+}
+
+int print_t(Top_List * tree)
+{
+	Internal_Node * current = tree->head->internal;
+    char s[20][255];
+    for (int i = 0; i < 20; i++)
+        sprintf(s[i], "%80s", " ");
+
+	/// Get to the root
+	while (current != NULL)
+		current = current->parent;
+
+    _print_t(current, 0, 0, 0, s);
+
+    for (int i = 0; i < 20; i++)
+        printf("%s\n", s[i]);
+}
