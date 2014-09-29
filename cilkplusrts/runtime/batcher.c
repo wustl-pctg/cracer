e6e74eae601f05c667dad05d995c4a294f367b41
Tom Wilkinson <tbondwilkinson@gmail.com>
2014-09-29 15:49:01 +0000

    Changes to batcher.c

--- cilkplusrts/runtime/batcher.c
+++ cilkplusrts/runtime/batcher.c
@@ -9,7 +9,24 @@
 #include <cilk/batcher.h>
 #include "rts-common.h"
 
+#include <stdio.h>
+
 CILK_API_VOID cilk_batchify(function_t f)
 {
-	f();
+	printf("Doing the function");
+	printf("Getting cilk worker");
+	CILK_ABI(__cilkrts_worker_ptr) w = __cilkrts_get_tls_worker();
+    cilk_fiber *fiber = NULL;
+    printf("Allocating new fiber");
+	START_INTERVAL(w, INTERVAL_FIBER_ALLOCATE) {
+        fiber = cilk_fiber_allocate(&w->l->fiber_pool);
+    } STOP_INTERVAL(w, INTERVAL_FIBER_ALLOCATE);
+    if (fiber == NULL) {
+    	printf("Could not get a fiber!");
+    } else {
+        full_frame *ff = __cilkrts_make_full_frame(w, 0);
+        ff->join_counter = 1;
+        w->l->frame_ff = ff;
+        f();
+    }
 }
