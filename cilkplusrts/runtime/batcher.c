ec6d7e8603cad992afd81f53535d029e5102acfc
Robert Utterback <robertu13@gmail.com>
2014-10-14 01:15:34 +0000

    Removed my silly little  hack and made some notes about the way batcher /should/ work

--- cilkplusrts/runtime/batcher.c
+++ cilkplusrts/runtime/batcher.c
@@ -16,22 +16,65 @@
 
 #include <stdio.h>
 
+COMMON_PORTABLE
+void internal_batchify(cilk_fiber *fiber)
+{
+  printf("In internal batchify\n");
+}
+
+/* Here's the plan for batchify:
+ *  Every worker allocated a new fiber, set to enter a batch
+ *  scheduling loop. The old fiber is set as the new "scheduling" fiber.
+ *  At the end of this batch scheduler, we suspend that fiber (destroy
+ *  it), and jump back to this fiber, where we return back to user
+ *  code.
+
+ * The batch owner must do something slightly different. There are two
+ * options:
+ *  1. Allocate an additional fiber. Jump to this fiber and execute an
+ * "internal batchify" function which just calls the batch operation
+ * (could also do collection and termination in there). That fiber is
+ * then destroyed and we jump to the batch scheduling fiber, where we
+ * just immediately destroy that and return back to user code.
+ *
+ * 2. Another option is to not allocate an additional fiber, and just
+ * manually call the internal batchify operation. This avoids the
+ * fiber overhead, but then we must do more careful accounting. In
+ * particular, I would be a little concerned about the fiber running
+ * the batch sharing a stack with a worker that stole something from
+ * the batch owner's core deque. Although it may not actually be
+ * anything to worry about.
+ *
+ * I think I'll implement #1 first, because it seems more straightforward.
+ */
 CILK_API_VOID cilk_batchify(function_t f)
 {
-	printf("Doing the function");
-	printf("Getting cilk worker");
+	printf("Doing the function\n");
+	printf("Getting cilk worker\n");
 	__cilkrts_worker * w = __cilkrts_get_tls_worker();
-    cilk_fiber *fiber = NULL;
-    printf("Allocating new fiber");
+  cilk_fiber *batch_fiber = NULL;
+  cilk_fiber *current_fiber = w->l->frame_ff->fiber_self;
+
+  printf("Allocating new fiber\n");
 	START_INTERVAL(w, INTERVAL_FIBER_ALLOCATE) {
-        fiber = cilk_fiber_allocate(&w->l->fiber_pool);
-    } STOP_INTERVAL(w, INTERVAL_FIBER_ALLOCATE);
-    if (fiber == NULL) {
-    	printf("Could not get a fiber!");
-    } else {
-        full_frame *ff = __cilkrts_make_full_frame(w, 0);
-        ff->join_counter = 1;
-        w->l->frame_ff = ff;
-        f();
-    }
+    batch_fiber = cilk_fiber_allocate(&w->l->fiber_pool); // allocate_from_thread? TODO(rob)
+    cilk_fiber_reset_state(batch_fiber, internal_batchify);
+    cilk_fiber_set_owner(batch_fiber, w);
+  } STOP_INTERVAL(w, INTERVAL_FIBER_ALLOCATE);
+
+  if (batch_fiber == NULL) {
+
+    // Should manually try to get lock and run batch sequentially?
+    printf("Could not get a fiber!\n");
+
+  } else {
+    /* full_frame *ff = __cilkrts_make_full_frame(w, 0); */
+    /* ff->join_counter = 1; */
+    /* w->l->frame_ff = ff; */
+    /* f(); */
+
+    cilk_fiber_suspend_self_and_resume_other(current_fiber, batch_fiber);
+  }
+
+  // suspend_self_and_resume_other
 }
--- cilkplusrts/runtime/full_frame.c
+++ cilkplusrts/runtime/full_frame.c
@@ -43,134 +43,134 @@ COMMON_PORTABLE
 full_frame *__cilkrts_make_full_frame(__cilkrts_worker *w,
                                       __cilkrts_stack_frame *sf)
 {
-    full_frame *ff;
+  full_frame *ff;
 
-    START_INTERVAL(w, INTERVAL_ALLOC_FULL_FRAME) {
-        ff = (full_frame *)__cilkrts_frame_malloc(w, sizeof(*ff));
-        __cilkrts_mutex_init(&ff->lock);
+  START_INTERVAL(w, INTERVAL_ALLOC_FULL_FRAME) {
+    ff = (full_frame *)__cilkrts_frame_malloc(w, sizeof(*ff));
+    __cilkrts_mutex_init(&ff->lock);
 
-        ff->full_frame_magic_0 = FULL_FRAME_MAGIC_0;
-        ff->join_counter = 0;
-        ff->parent = 0;
-        ff->rightmost_child = 0;
-        ff->left_sibling = ff->right_sibling = 0;
-        ff->call_stack = sf;
-        ff->is_call_child = 0;
-        ff->simulated_stolen = 0;
-	ff->children_reducer_map = ff->right_reducer_map = 0;
-        ff->pending_exception = 
-            ff->child_pending_exception = 
-            ff->right_pending_exception = NULL;
+    ff->full_frame_magic_0 = FULL_FRAME_MAGIC_0;
+    ff->join_counter = 0;
+    ff->parent = 0;
+    ff->rightmost_child = 0;
+    ff->left_sibling = ff->right_sibling = 0;
+    ff->call_stack = sf;
+    ff->is_call_child = 0;
+    ff->simulated_stolen = 0;
+    ff->children_reducer_map = ff->right_reducer_map = 0;
+    ff->pending_exception = 
+      ff->child_pending_exception = 
+      ff->right_pending_exception = NULL;
 
-        ff->sync_sp = 0;
+    ff->sync_sp = 0;
 #ifdef _WIN32
-        ff->exception_sp = 0;
-        ff->trylevel = (unsigned long)-1;
-        ff->registration = 0;
+    ff->exception_sp = 0;
+    ff->trylevel = (unsigned long)-1;
+    ff->registration = 0;
 #endif
-	ff->frame_size = 0;
-        ff->fiber_self = 0;
-        ff->fiber_child = 0;
+    ff->frame_size = 0;
+    ff->fiber_self = 0;
+    ff->fiber_child = 0;
 
-        ff->sync_master = 0;
+    ff->sync_master = 0;
 
-        /*__cilkrts_init_full_frame_sysdep(w, ff);*/
-        ff->full_frame_magic_1 = FULL_FRAME_MAGIC_1;
-    } STOP_INTERVAL(w, INTERVAL_ALLOC_FULL_FRAME);
-    return ff;
+    /*__cilkrts_init_full_frame_sysdep(w, ff);*/
+    ff->full_frame_magic_1 = FULL_FRAME_MAGIC_1;
+  } STOP_INTERVAL(w, INTERVAL_ALLOC_FULL_FRAME);
+  return ff;
 }
 
 COMMON_PORTABLE void __cilkrts_put_stack(full_frame *ff,
                                          __cilkrts_stack_frame *sf)
 {
-    /* When suspending frame ff prior to stealing it, __cilkrts_put_stack is
-     * used to store the stack pointer for eventual sync.  When suspending
-     * frame ff prior to a sync, __cilkrts_put_stack is called to re-establish
-     * the sync stack pointer, offsetting it by any change in the stack depth
-     * that occured between the spawn and the sync.
-     * Although it is not usually meaningful to add two pointers, the value of
-     * ff->sync_sp at the time of this call is really an integer, not a
-     * pointer.
-     */
-    ptrdiff_t sync_sp_i = (ptrdiff_t) ff->sync_sp;
-    char* sp = (char*) __cilkrts_get_sp(sf);
+  /* When suspending frame ff prior to stealing it, __cilkrts_put_stack is
+   * used to store the stack pointer for eventual sync.  When suspending
+   * frame ff prior to a sync, __cilkrts_put_stack is called to re-establish
+   * the sync stack pointer, offsetting it by any change in the stack depth
+   * that occured between the spawn and the sync.
+   * Although it is not usually meaningful to add two pointers, the value of
+   * ff->sync_sp at the time of this call is really an integer, not a
+   * pointer.
+   */
+  ptrdiff_t sync_sp_i = (ptrdiff_t) ff->sync_sp;
+  char* sp = (char*) __cilkrts_get_sp(sf);
 
-    ff->sync_sp = sp + sync_sp_i;
+  ff->sync_sp = sp + sync_sp_i;
 
-    DBGPRINTF("%d-                __cilkrts_put_stack - adjust (+) sync "
-              "stack of full frame %p (+sp: %p) to %p\n",
-              __cilkrts_get_tls_worker()->self, ff, sp, ff->sync_sp);
+  DBGPRINTF("%d-                __cilkrts_put_stack - adjust (+) sync "
+            "stack of full frame %p (+sp: %p) to %p\n",
+            __cilkrts_get_tls_worker()->self, ff, sp, ff->sync_sp);
 }
 
 COMMON_PORTABLE void __cilkrts_take_stack(full_frame *ff, void *sp)
 {
-    /* When resuming the parent after a steal, __cilkrts_take_stack is used to
-     * subtract the new stack pointer from the current stack pointer, storing
-     * the offset in ff->sync_sp.  When resuming after a sync,
-     * __cilkrts_take_stack is used to subtract the new stack pointer from
-     * itself, leaving ff->sync_sp at zero (null).  Although the pointers being
-     * subtracted are not part of the same contiguous chunk of memory, the
-     * flat memory model allows us to subtract them and get a useable offset.
-     */
-    ptrdiff_t sync_sp_i = ff->sync_sp - (char*) sp;
+  /* When resuming the parent after a steal, __cilkrts_take_stack is used to
+   * subtract the new stack pointer from the current stack pointer, storing
+   * the offset in ff->sync_sp.  When resuming after a sync,
+   * __cilkrts_take_stack is used to subtract the new stack pointer from
+   * itself, leaving ff->sync_sp at zero (null).  Although the pointers being
+   * subtracted are not part of the same contiguous chunk of memory, the
+   * flat memory model allows us to subtract them and get a useable offset.
+   */
+  ptrdiff_t sync_sp_i = ff->sync_sp - (char*) sp;
 
-    ff->sync_sp = (char *) sync_sp_i;
+  ff->sync_sp = (char *) sync_sp_i;
 
-    DBGPRINTF("%d-                __cilkrts_take_stack - adjust (-) sync "
-              "stack of full frame %p to %p (-sp: %p)\n",
-              __cilkrts_get_tls_worker()->self, ff, ff->sync_sp, sp);
+  DBGPRINTF("%d-                __cilkrts_take_stack - adjust (-) sync "
+            "stack of full frame %p to %p (-sp: %p)\n",
+            __cilkrts_get_tls_worker()->self, ff, ff->sync_sp, sp);
 }
 
 COMMON_PORTABLE void __cilkrts_adjust_stack(full_frame *ff, size_t size)
 {
-    /* When resuming the parent after a steal, __cilkrts_take_stack is used to
-     * subtract the new stack pointer from the current stack pointer, storing
-     * the offset in ff->sync_sp.  When resuming after a sync,
-     * __cilkrts_take_stack is used to subtract the new stack pointer from
-     * itself, leaving ff->sync_sp at zero (null).  Although the pointers being
-     * subtracted are not part of the same contiguous chunk of memory, the
-     * flat memory model allows us to subtract them and get a useable offset.
-     *
-     * __cilkrts_adjust_stack() is used to deallocate a Variable Length Array
-     * by adding it's size to ff->sync_sp.
-     */
-    ff->sync_sp = ff->sync_sp + size;
+  /* When resuming the parent after a steal, __cilkrts_take_stack is used to
+   * subtract the new stack pointer from the current stack pointer, storing
+   * the offset in ff->sync_sp.  When resuming after a sync,
+   * __cilkrts_take_stack is used to subtract the new stack pointer from
+   * itself, leaving ff->sync_sp at zero (null).  Although the pointers being
+   * subtracted are not part of the same contiguous chunk of memory, the
+   * flat memory model allows us to subtract them and get a useable offset.
+   *
+   * __cilkrts_adjust_stack() is used to deallocate a Variable Length Array
+   * by adding it's size to ff->sync_sp.
+   */
+  ff->sync_sp = ff->sync_sp + size;
 
-    DBGPRINTF("%d-                __cilkrts_adjust_stack - adjust (+) sync "
-              "stack of full frame %p to %p (+ size: 0x%x)\n",
-              __cilkrts_get_tls_worker()->self, ff, ff->sync_sp, size);
+  DBGPRINTF("%d-                __cilkrts_adjust_stack - adjust (+) sync "
+            "stack of full frame %p to %p (+ size: 0x%x)\n",
+            __cilkrts_get_tls_worker()->self, ff, ff->sync_sp, size);
 }
 
 COMMON_PORTABLE
 void __cilkrts_destroy_full_frame(__cilkrts_worker *w, full_frame *ff)
 {
-    validate_full_frame(ff);
-    CILK_ASSERT(ff->children_reducer_map == 0);
-    CILK_ASSERT(ff->right_reducer_map == 0);
-    CILK_ASSERT(NULL == ff->pending_exception);
-    CILK_ASSERT(NULL == ff->child_pending_exception);
-    CILK_ASSERT(NULL == ff->right_pending_exception);
-    __cilkrts_mutex_destroy(w, &ff->lock);
-    __cilkrts_frame_free(w, ff, sizeof(*ff));
+  validate_full_frame(ff);
+  CILK_ASSERT(ff->children_reducer_map == 0);
+  CILK_ASSERT(ff->right_reducer_map == 0);
+  CILK_ASSERT(NULL == ff->pending_exception);
+  CILK_ASSERT(NULL == ff->child_pending_exception);
+  CILK_ASSERT(NULL == ff->right_pending_exception);
+  __cilkrts_mutex_destroy(w, &ff->lock);
+  __cilkrts_frame_free(w, ff, sizeof(*ff));
 }
 
 COMMON_PORTABLE void validate_full_frame(full_frame *ff)
 {
-    /* check the magic numbers, for debugging purposes */
-    if (ff->full_frame_magic_0 != FULL_FRAME_MAGIC_0 ||
-        ff->full_frame_magic_1 != FULL_FRAME_MAGIC_1)
-        abort_because_rts_is_corrupted();
+  /* check the magic numbers, for debugging purposes */
+  if (ff->full_frame_magic_0 != FULL_FRAME_MAGIC_0 ||
+      ff->full_frame_magic_1 != FULL_FRAME_MAGIC_1)
+    abort_because_rts_is_corrupted();
 }
 
 void __cilkrts_frame_lock(__cilkrts_worker *w, full_frame *ff)
 {
-    validate_full_frame(ff);
-    __cilkrts_mutex_lock(w, &ff->lock);
+  validate_full_frame(ff);
+  __cilkrts_mutex_lock(w, &ff->lock);
 }
 
 void __cilkrts_frame_unlock(__cilkrts_worker *w, full_frame *ff)
 {
-    __cilkrts_mutex_unlock(w, &ff->lock);
+  __cilkrts_mutex_unlock(w, &ff->lock);
 }
 
 /* End full_frame.c */
