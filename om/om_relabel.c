bd5f3968dc2f07ddb7f004a98ba6ad4d005372da
Robert Utterback <robertu13@gmail.com>
2015-08-17 14:20:09 -0500

    Relabel uses list of new split nodes.
    Rather than using an array of new split nodes for each split leaf, the
    relabel (specifically, the build_array_of_leaves function) function now
    uses the list provided by split, rather than an array.
    
    Note that I do not patch the new lists into the list of leaves. I've
    made a corresponding note of this in om.c. This is because it would be a
    little extra work because naively doing this results in a race condition
    between freeing the old tl_nodes and using the next/prev pointers. I'm
    pretty sure we could get around this, but I don't see any reason to
    because we're not using the list of leaves for any purpose other than
    relabeling after splitting.

--- om/om_relabel.c
+++ om/om_relabel.c
@@ -1,225 +1,215 @@
 // The relabel-specific code for order-maintenance.
 
+#include <string.h> // memcpy
+
+#include "blist_split.c"
+
 #define FAST_LOG2(x) (sizeof(label_t)*8 - 1 - __builtin_clzl((label_t)(x)))
 #define FAST_LOG2_CEIL(x) (((x) - (1 << FAST_LOG2(x))) ? FAST_LOG2(x) + 1 : FAST_LOG2(x))
+#define parfor for
+#define spawn
+#define sync
+
+static inline double density(tl_node* n)
+{
+  /// @todo I've changed the size to mean the number of leaves...does
+  /// this change this formula?
+  return n->size / (double)(1 << (MAX_LEVEL - n->level));
+}
 
 int too_heavy(tl_node* n, size_t height)
 {
   double threshold = 0.75 + 0.25 * (n->level / (double)height );
-  double density = n->size / (1 << (MAX_LEVEL - n->level));
-  return density <= threshold;
+  return density(n) >= threshold;
 }
 
-void rebuild_recursive(tl_node* n, tl_node** array, int left, int right);
-
-void build_array_of_leaves(tl_node* n, tl_node** array,
+tl_node* rebuild_recursive(tl_node* n, tl_node** array,
                            size_t left_index, size_t right_index,
-                           label_t left_label, label_t right_label)
+                           label_t left_lab, label_t right_lab);
+
+label_t build_array_of_leaves(tl_node* n, tl_node** array,
+                           size_t left_index, size_t right_index)
 {
-  size_t max_size = 1 << n->level;
-  label_t spacing = max_size / n->size;
-  assert(n->left);
-  
-  if (n->left->level < MAX_LEVEL) {
-    size_t index_end = (n->right) ? right_index - n->right->size : right_index;
-    label_t label_end = (n->right) ? right_label - (n->right->size * spacing) : right_label;
-    build_array_of_leaves(n->left, array,
-                          left_index, index_end,
-                          left_label, label_end);
-    tl_node_free(n->left);
+  if (!n) return 0;
+  label_t left_most_label;
+
+  if (n->level == MAX_LEVEL) {
+    left_most_label = n->label;
+    size_t index = left_index;
+    if (n->size == 1) {
+      array[index] = n;
+      assert(index + 1 == right_index);
+    } else {
+      tl_node* current = n->split_nodes;
+
+      while (current) {
+        array[index] = current;
+        current = current->next;
+        index++;
+      }
+      assert(index == right_index);
+    }
   } else {
-    array[left_index] = n->left;
-    n->left->label = left_label;
+    assert(n->left);
+    size_t mid = left_index + n->left->size;
+    left_most_label = spawn build_array_of_leaves(n->left, array,
+                                                  left_index, mid);
+    build_array_of_leaves(n->right, array, mid, right_index);
+    sync;
+    n->left = n->right = NULL;
   }
 
-  if (!n->right) return;
-
-  if (n->right->level < MAX_LEVEL) {
-    size_t index_start = left_index + n->left->size + 1;
-    label_t label_start = left_index + (n->left->size * spacing) + 1;
-    build_array_of_leaves(n->right, array,
-                          index_start, right_index,
-                          label_start, right_label);
-    tl_node_free(n->right);
-  } else {
-    array[right_index] = n->right;
-    n->right->label = right_label;
-  }
+  // We don't want to free the root of this subtree...
+  if (n->needs_rebalance) tl_node_free(n);
+  return left_most_label;
 }
 
 // Rebuild a subtree rooted at node n.
-void rebuild(tl_node* n)
+void rebuild_subtree(tl_node* n)
 {
-  tl_node** array = malloc(n->size * sizeof(tl_node*));
-
+  assert(n->level < MAX_LEVEL);
+  assert(n->needs_rebalance == 0);
+  size_t array_size = n->size;
+  tl_node** array = malloc(array_size * sizeof(tl_node*));
+  
   // will remove extra scaffolding
-  build_array_of_leaves(n, array, 0, n->size,
-                        0, MAX_LABEL);
+  label_t left_lab = build_array_of_leaves(n, array, 0, array_size);
+  n->left = n->right = NULL;
+  
+  label_t right_lab = n->label + 1;
+  label_t spacing = (right_lab - left_lab) / array_size;
+  
+  size_t mid_index = array_size / 2;
+  label_t mid_lab = left_lab + spacing * mid_index;
 
-  // Now we can either build bottom up or top down.
-  // Top down
-  rebuild_recursive(n, array, 0, n->size);
+  n->left = spawn rebuild_recursive(n, array, 0, mid_index,
+                                    left_lab, mid_lab);
+  n->right = rebuild_recursive(n, array, mid_index, array_size,
+                               mid_lab, right_lab);
+  sync;
+  free(array);
 }
 
-void rebuild_recursive(tl_node* n, tl_node** array, int left, int right)
+tl_node* rebuild_recursive(tl_node* parent, tl_node** array,
+                           size_t left_index, size_t right_index,
+                           label_t left_lab, label_t right_lab)
 {
-  assert(n->left == NULL && n->right == NULL);
+  int size = right_index - left_index;
+  tl_node* n = (size == 1) ? array[left_index] : tl_node_new();
+  n->size = size;
+  n->parent = parent;
+  n->needs_rebalance = 0;
 
-  int range = right - left;
-  if (range > 2) {
-    n->left = tl_node_new(); /// @todo do these need initialization?
-    n->left->needs_rebalance = 0;
-    n->right = tl_node_new();
-    n->right->needs_rebalance = 0;
-    
-    rebuild_recursive(n->left, array, left, right / 2);
-    rebuild_recursive(n->right, array, (right / 2) + 1, right);
-  } else {
-    n->left = array[left];
-    if (left != right) n->right = array[right];
-  }
-
-}
-
-  // 3. Walk down again in parallel until we find that density is bad
-  //    Then rebalance, similar to the lower-level rebalance
-  //    Parallel prefix (to get ptr to leaf) on leaves (we have an
-  //    array of these)
-void recursive_rebalance(tl_node* n)
-{
-  if (n->level < MAX_LEVEL && n->needs_rebalance) {
-    n->needs_rebalance = 0;
-    if (n->left->needs_rebalance || n->right->needs_rebalance) {
-      /*spawn*/ recursive_rebalance(n->left);
-      recursive_rebalance(n->right);
-      /*sync;*/
-    } else {
-      rebuild(n);
+  if (size == 1) {
+    assert(left_lab < right_lab);
+    n->label = left_lab;
+    n->level = MAX_LEVEL;
+    if (left_index > 0) {
+      assert(n->label > 0);
+      n->prev = array[left_index - 1];
+      array[left_index - 1]->next = n;
     }
-  }
-}
-
-
-// Assume lower-level tries have been split and already inserted into
-// the top-level trie
-void rebalance(om* self)
-{
-  // Theoretical version:
-  // 1. Walk up the tree, marking that paths have changed (not exact
-  // sizes -- would need some lock free protocol)
-  // 2. Walk down the marked paths in parallel, getting correct counts
-  //
-  // Practical version:
-  // 1&2: Walk up tree, updating sizes
-
-  int num_split = g_marked_array_size;
-  /*parallel*/ for (int i = 0; i < num_split; ++i) {
-    tl_node* current = g_marked_array[i];
-    size_t additional_size = current->size - 1;
-    
-    current = current->parent;
-    while (current) {
-      __sync_fetch_and_add(&current->size, additional_size);
-      if (too_heavy(current, self->height)) current->needs_rebalance = 1;
-      current = current->parent;
-    }
-  }
-
-
-  // 3. Walk down again in parallel until we find that density is bad
-  //    Then rebalance, similar to the lower-level rebalance
-  //    Parallel prefix (to get ptr to leaf) on leaves (we have an
-  //    array of these)
-  recursive_rebalance(self->root);
-
-  /// @todo update height values!
-}
-
-#define LEFT 0
-#define RIGHT 1
-
-tl_node* insert_top(blist** sublists, size_t start, size_t end, tl_node* top,
-                    unsigned char dir_flag)
-{
-  tl_node* node = tl_node_new();
-  node->parent = top;
-  node->size = end - start + 1;
-
-  if (dir_flag == LEFT) {
-    int bit_shifts = MAX_LEVEL - top->level;
-    node->label = ((top->label >> (bit_shifts)) << bit_shifts);
-    if (start != end) node->label++;
   } else {
-    node->label = top->label;
+    n->level = n->parent->level + 1;;
+
+    size_t mid_index = left_index + size / 2;
+    label_t spacing = (right_lab - left_lab) / size;
+    label_t mid_lab = left_lab + spacing * (mid_index - left_index);
+    n->left = spawn rebuild_recursive(n, array, left_index, mid_index,
+                                            left_lab, mid_lab);
+    n->right = rebuild_recursive(n, array, mid_index, right_index,
+                                       mid_lab, right_lab);
+    sync;
+    
+    n->label = array[right_index - 1]->label;
   }
   
-  if (start == end) {
-    node->level = MAX_LEVEL;
-    node->below = sublists[start];
+  return n;
+}
 
-    node->needs_rebalance = 0;
-
-    // Patch list of leaves
-    sublists[start]->above = node;
-    node->prev = NULL;
-    node->next = NULL;
-    if (start != 0) {
-      node->prev = sublists[start - 1]->above;
-      sublists[start - 1]->above->next = node;
-    }
-  } else {
-    node->level = top->level + 1;
-    node->below = NULL;
-    node->needs_rebalance = 0; ///@todo mark this here if necessary?
-
-    node->left = insert_top(sublists, start, end / 2, node, LEFT);
-    node->right = insert_top(sublists, (end / 2) + 1, end, node, RIGHT);
+void rebuild_leaf(tl_node* n)
+{
+  assert(n->level == MAX_LEVEL && n->parent && n->size > 1);
+  assert(n->left == NULL && n->right == NULL);
+  label_t left_lab = n->label;
+  label_t right_lab;
+  if (n->parent->right == n) right_lab = n->parent->label;
+  else {
+    right_lab = left_lab + ((n->parent->label - left_lab) / 2);
   }
-  return node;
+  assert(n->size == right_lab - left_lab + 1);
+
+  size_t array_size = n->size;
+  tl_node** array = malloc(array_size * sizeof(tl_node*));
+  
+  // will remove extra scaffolding
+  assert(left_lab == build_array_of_leaves(n, array, 0, array_size));
+  
+  // Change this node to be an internal node.
+  n->label = right_lab;
+  n->level = n->parent->level + 1;
+  
+  right_lab++; // we pass in a higher label than we will use.
+  label_t spacing = (right_lab - left_lab) / n->size;
+  size_t mid_index = n->size / 2;
+  label_t mid_lab = left_lab + spacing * mid_index;
+
+  n->left = spawn rebuild_recursive(n, array, 0, mid_index,
+                                    left_lab, mid_lab);
+  n->right = rebuild_recursive(n, array, mid_index, n->size,
+                               mid_lab + spacing, right_lab + 1);
+  sync;
+
+}
+
+void recursive_rebalance(tl_node* n, size_t height)
+{
+  if (!n || !n->needs_rebalance) return; // stop here
+  n->needs_rebalance = 0;
+
+  if (n->level == MAX_LEVEL) return rebuild_leaf(n);
+  assert(n->left);
+  if (too_heavy(n, height)) return rebuild_subtree(n);
+  
+  spawn recursive_rebalance(n->left, height);
+  recursive_rebalance(n->right, height);
+}
+
+
+static inline void rebalance(om* self)
+{
+  recursive_rebalance(self->root, self->height);
 }
 
 void relabel(om* self, tl_node** heavy_lists, size_t num_heavy_lists)
 {
-  for (int i = 0; i < num_heavy_lists; ++i) {
-    blist** new_lists;
-
-    // Split into several sublists.
-    size_t array_size = split(heavy_lists[i]->below, &new_lists);
-
-    // Insert new sublists into top level.
-    tl_node* old = heavy_lists[i];
-    assert(old->level == MAX_LEVEL);
-    tl_node* parent = old->parent;
-    if (old->prev) old->prev->next = old->next;
-    if (old->next) old->next->prev = old->prev;
-
-    unsigned char direction = (parent->right == old);
-    size_t height = FAST_LOG2_CEIL(array_size);
-    if (direction == LEFT && !parent->right) {
-      size_t middle = (array_size - 1) / 2;
-      parent->left = insert_top(new_lists, 0, middle, parent, LEFT);
-      parent->right = insert_top(new_lists, middle + 1, array_size - 1,
-                                 parent, RIGHT);
-      height--;
-    } else {
-      insert_top(new_lists, 0, array_size - 1, parent, direction);
-    }
+  parfor (int i = 0; i < num_heavy_lists; ++i) {
+    tl_node* current = heavy_lists[i];
     
-    while (parent) {
-      //parent->size += array_size - 1;
-      __sync_fetch_and_add(&parent->size, array_size - 1);
+    // Split into several sublists.
+    size_t num_split_lists = split(current->below);//, &new_lists);
+    
+    // We can tell a node is split by its level being MAX_LEVEL and it
+    // needs rebalance
+    assert(current->level == MAX_LEVEL);
+
+    // Update sizes up the tree, while also calculating the height
+    // from the current node.
+    
+    // -1 because we increment it below for the current node
+    size_t height = FAST_LOG2_CEIL(num_split_lists) - 1;
+    while (current) {
+      __sync_fetch_and_add(&current->size, num_split_lists - 1);
+      current->needs_rebalance = 1;
       height++;
-      parent = parent->parent;
+      current = current->parent;
     }
 
+    // Update the height, if necessary.
     size_t old_height = self->height;
     while (height > old_height) {
-      old_height = __sync_val_compare_and_swap(&self->height, old_height, height);
+        old_height = __sync_val_compare_and_swap(&self->height, old_height, height);
     }
-    
-    tl_node_free(old);
-    free(new_lists);
   }
-
   rebalance(self);
 }
