2da162b0deee6c6110061188db9824309d417be5
Robert Utterback <robertu13@gmail.com>
2015-08-24 16:47:42 -0500

    Array of stacks of frames.
    Allocated and use an array of stacks of frames now, rather than just
    one. This is a start on inserting into the OM ds in parallel.

--- rdtool/omrd.cpp
+++ rdtool/omrd.cpp
@@ -22,7 +22,7 @@ typedef struct FrameData_s {
   frame_t type;
 } FrameData_t;
 
-Stack_t<FrameData_t> frames;
+Stack_t<FrameData_t>* frames;
 
 /// @todo A different design would be to have each OM itself contain a
 /// list of heavy nodes to rebalance.
@@ -30,17 +30,15 @@ Stack_t<tl_node*> heavy_english;
 Stack_t<tl_node*> heavy_hebrew;
 
 #define HALF_BITS ((sizeof(label_t) * 8) / 2)
-//#define HALF_BITS 1
 
-void init_strand()
+void init_strand(int worker_id)
 {
   //  std::cout << "initial push\n";
-  assert(frames.size() == 1);
-  FrameData_t* f = frames.head();
+  assert(frames[worker_id].size() == 1);
+  FrameData_t* f = frames[worker_id].head();
   f->type = USER;
   f->current_english = om_insert_initial(g_english);
   f->current_hebrew = om_insert_initial(g_hebrew);
-
   f->cont_english = NULL;
   f->cont_hebrew = NULL;
   f->sync_english = NULL;
@@ -70,25 +68,18 @@ om_node* insert_or_relabel(__cilkrts_worker* w, om* ds,
   om_node* n = om_insert(ds, base);
   while (!n) {
     if (!base->list->half_full) {
-      //      printf("Adding list of %p to marked array.\n", base);
       *heavy_nodes.head() = om_get_tl(base);
       heavy_nodes.push();
     }
 
-    //    printf("relabel.\n");
     om_relabel(ds, heavy_nodes.at(0), heavy_nodes.size() - 1);
-    if (base->list->above->level != MAX_LEVEL) {
-      //      printf("base level: %zu\n", base->list->above->level);
-    }
     heavy_nodes.reset();
 
     n = om_insert(ds, base);
   }
   if (!n->list->half_full
-      && count_set_bits(n->label) >= HALF_BITS) { // uses half
-                                                  // available bits
+      && count_set_bits(n->label) >= HALF_BITS) {
     n->list->half_full = 1;
-    //    printf("List of %p is half full.\n", base);
     *heavy_nodes.head() = om_get_tl(n);
     heavy_nodes.push();
   }
@@ -97,64 +88,57 @@ om_node* insert_or_relabel(__cilkrts_worker* w, om* ds,
 
 extern "C" void cilk_tool_init(void) 
 { 
-  // std::cout << __FUNCTION__ << std::endl;
   g_tool_init = 1;
   g_english = om_new();
   g_hebrew = om_new();
+  frames = new Stack_t<FrameData_t>[__cilkrts_get_nworkers()];
 
-  assert(0);
-
-  init_strand();
-
+  init_strand(__cilkrts_get_worker_number());
   /// @todo allocate worker locks
 }
 
 // Currently, I don't think this is ever called. @todo
 extern "C" void cilk_tool_destroy(void) 
 { 
-  // std::cout << __FUNCTION__ << std::endl;
   om_free(g_english);
   om_free(g_hebrew);
+
+  delete[] frames;
 }
 
 extern "C" void cilk_tool_print(void) { }
 
 extern "C" void cilk_tool_c_function_enter(void* this_fn, void* rip)
 { 
-  // std::cout << __FUNCTION__ << std::endl;
-  if (frames.size() > 1) assert(frames.head()->type == USER);
+  int id = __cilkrts_get_worker_number();
+  if (frames[id].size() > 1) assert(frames[id].head()->type == USER);
 }
-extern "C" void cilk_tool_c_function_leave(void* rip) 
-{ 
-  // std::cout << __FUNCTION__ << std::endl;
-}
+extern "C" void cilk_tool_c_function_leave(void* rip) { }
 
 extern "C" void cilk_enter_begin(__cilkrts_stack_frame* sf, void* this_fn, void* rip)
 {
-  // std::cout << __FUNCTION__ << std::endl;
+  /// Need to call it like this because sf->worker may not be
+  /// correctly initialized.
   __cilkrts_worker* w = __cilkrts_get_tls_worker();
-  if (w && __cilkrts_get_batch_id(__cilkrts_get_tls_worker()) != -1) return;
+  if (w && __cilkrts_get_batch_id(w) != -1) return;
 
   if (!g_tool_init) cilk_tool_init();
-  else assert(frames.head()->type == USER);
-
-  //  if (frames.size() == 1) init_strand(); // actually, check if empty...
+  else if (w) assert(frames[w->self].head()->type == USER);
 }
 
 extern "C" void cilk_enter_helper_begin(__cilkrts_stack_frame* sf, void* this_fn, void* rip)
 { 
-  // std::cout << __FUNCTION__ << "  push 2" << std::endl;
   __cilkrts_worker* w = __cilkrts_get_tls_worker();
   if (__cilkrts_get_batch_id(w) != -1) return;
-  assert(frames.size() > 0); // want non-empty, but stack returns
+  assert(frames[w->self].size() > 0); // want non-empty, but stack returns
                              // _head + 1
 
-  FrameData_t* parent = frames.head(); // helper frame parent
+  FrameData_t* parent = frames[w->self].head(); // helper frame parent
   assert(parent->type == USER);
-  frames.push(); frames.head()->type = HELPER;
+  frames[w->self].push(); frames[w->self].head()->type = HELPER;
 
-  frames.push();
-  FrameData_t* f = frames.head();
+  frames[w->self].push();
+  FrameData_t* f = frames[w->self].head();
   f->type = USER;
 
   if (!parent->sync_english) { // first of spawn group
@@ -180,46 +164,25 @@ extern "C" void cilk_enter_helper_begin(__cilkrts_stack_frame* sf, void* this_fn
   parent->current_english = NULL; parent->current_hebrew = NULL;
 }
 
-extern "C" void cilk_enter_end (__cilkrts_stack_frame* sf, void* rsp)
-{ 
-  // std::cout << __FUNCTION__ << std::endl;
-  if (__cilkrts_get_batch_id(sf->worker) != -1) return;
-}
+extern "C" void cilk_enter_end (__cilkrts_stack_frame* sf, void* rsp) { }
 
-extern "C" void cilk_spawn_prepare (__cilkrts_stack_frame* sf)
-{ 
-  // std::cout << __FUNCTION__ << std::endl;
-  if (__cilkrts_get_batch_id(sf->worker) != -1) return;
-}
+extern "C" void cilk_spawn_prepare (__cilkrts_stack_frame* sf) { }
 
 extern "C" void cilk_spawn_or_continue (int in_continuation)
 { 
-  // std::cout << __FUNCTION__ << std::endl;
   assert(!in_continuation);
 }
-extern "C" void cilk_detach_begin (__cilkrts_stack_frame* parent)
-{ 
-  // std::cout << __FUNCTION__ << std::endl;
-  if (__cilkrts_get_batch_id(parent->worker) != -1) return;
-}
 
-extern "C" void cilk_detach_end (void)
-{ 
-  // std::cout << __FUNCTION__ << std::endl;
+extern "C" void cilk_detach_begin (__cilkrts_stack_frame* parent) { }
 
-}
+extern "C" void cilk_detach_end (void) { }
 
-extern "C" void cilk_sync_begin (__cilkrts_stack_frame* sf)
-{ 
-  // std::cout << __FUNCTION__ << std::endl;
-  if (__cilkrts_get_batch_id(sf->worker) != -1) return;
-}
+extern "C" void cilk_sync_begin (__cilkrts_stack_frame* sf) { }
 
 extern "C" void cilk_sync_end (__cilkrts_stack_frame* sf)
 { 
-  // std::cout << __FUNCTION__ << std::endl;
   if (__cilkrts_get_batch_id(sf->worker) != -1) return;
-  FrameData_t* f = frames.head();
+  FrameData_t* f = frames[sf->worker->self].head();
   assert(f->current_english); assert(f->current_hebrew);
   assert(!f->cont_english); assert(!f->cont_hebrew);
 
@@ -236,16 +199,15 @@ extern "C" void cilk_sync_end (__cilkrts_stack_frame* sf)
 
 extern "C" void cilk_leave_begin (__cilkrts_stack_frame *sf)
 { 
-  // std::cout << __FUNCTION__ << "   pop 1" << std::endl;
   if (__cilkrts_get_batch_id(sf->worker) != -1) return;
 
-  if (frames.size() == 1) { // empty
-    //    frames.pop();
+  if (frames[sf->worker->self].size() == 1) { // empty
+    //    frames[sf->worker->self].pop();
   } else {
-    frame_t type = frames.head()->type;
-    frames.pop();
+    frame_t type = frames[sf->worker->self].head()->type;
+    frames[sf->worker->self].pop();
     if (type == HELPER) return;
-    FrameData_t* current = frames.ancestor(1);
+    FrameData_t* current = frames[sf->worker->self].ancestor(1);
     assert(!current->current_english);
     assert(!current->current_hebrew);
     assert(current->cont_english);
