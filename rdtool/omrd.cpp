8ffde91a49f37212586ecf54135a56cc7e9a250d
Robert Utterback <robertu13@gmail.com>
2015-09-03 13:44:47 -0500

    Fixed handling of shadow stack.
    Changed the way I handled the shadow stack, which fixes the bug caused
    by some benchmarks (fib without the second spawn and matmul), as well
    as improves the runtime.

--- rdtool/omrd.cpp
+++ rdtool/omrd.cpp
@@ -4,8 +4,6 @@
 #include <cilk/batcher.h>
 #include "stack.h"
 
-int g_tool_init = 0;
-
 typedef enum {
   TOOL = 0,
   TOOL_INSERT,
@@ -23,8 +21,11 @@ typedef enum {
   NUM_INTERVAL_TYPES,
 } interval_types;
 
-unsigned long g_num_relabels = 0;
-unsigned long g_num_inserts = 0;
+#ifdef DEBUG
+#define RD_DEBUG(...) printf(__VA_ARGS__)
+#else
+#define RD_DEBUG(...)
+#endif
 
 #ifdef STATS
 #include <time.h>
@@ -45,12 +46,13 @@ struct timespec EMPTY_TIME_POINT;
 #define RDTOOL_INTERVAL_END(i)
 #endif // ifdef STATS
 
+int g_tool_init = 0;
+unsigned long g_num_relabels = 0;
+unsigned long g_num_inserts = 0;
 #include "../om/om.c"
 
 om* g_english;
 om* g_hebrew;
-
-typedef enum { USER, HELPER } frame_t;
 
 typedef struct FrameData_s {
   om_node* current_english;
@@ -59,7 +61,7 @@ typedef struct FrameData_s {
   om_node* cont_hebrew;
   om_node* sync_english;
   om_node* sync_hebrew;
-  frame_t type;
+  size_t enter_counter;
 } FrameData_t;
 
 Stack_t<FrameData_t>* frames;
@@ -74,12 +76,9 @@ Stack_t<tl_node*> heavy_hebrew;
 void init_strand(int worker_id)
 {
   assert(frames[worker_id].empty());
-  //  RDTOOL_INTERVAL_BEGIN(SHADOW_STACK_MANIP);
   frames[worker_id].push();
-  //  RDTOOL_INTERVAL_END(SHADOW_STACK_MANIP);
   FrameData_t* f = frames[worker_id].head();
-
-  f->type = USER;
+  f->enter_counter = 0;
   f->current_english = om_insert_initial(g_english); g_num_inserts++;
   f->current_hebrew = om_insert_initial(g_hebrew); g_num_inserts++;
   f->cont_english = NULL;
@@ -103,48 +102,33 @@ om_node* insert_or_relabel(__cilkrts_worker* w, om* ds,
                            Stack_t<tl_node*>& heavy_nodes, om_node* base)
 {
   g_num_inserts++;
-  //  RDTOOL_INTERVAL_BEGIN(TOOL_INSERT);
-  // Get worker om lock
-  //  RDTOOL_INTERVAL_BEGIN(OM_INSERT);
   om_node* n = om_insert(ds, base);
-  //  RDTOOL_INTERVAL_END(OM_INSERT);
   while (!n) {
-    //    RDTOOL_INTERVAL_BEGIN(HEAVY_ARRAY_MANIP);
     if (!base->list->half_full) {
-      //      printf("Could not fit into list of size %zu.\n", base->list->size);
+      RD_DEBUG("Could not fit into list of size %zu, not half full.\n", base->list->size);
       heavy_nodes.push();
       *heavy_nodes.head() = om_get_tl(base);
     }
-    //    RDTOOL_INTERVAL_END(HEAVY_ARRAY_MANIP);
-
-    //    RDTOOL_INTERVAL_BEGIN(OM_RELABEL);
-    om_relabel(ds, heavy_nodes.at(0), heavy_nodes.size());
     g_num_relabels++;
-    //    om_print(ds);
-    //    RDTOOL_INTERVAL_END(OM_RELABEL);
+    RD_DEBUG("Begin relabel %zu.\n", g_num_relabels)
+    om_relabel(ds, heavy_nodes.at(0), heavy_nodes.size());
     heavy_nodes.reset();
 
-    //    RDTOOL_INTERVAL_BEGIN(OM_INSERT);
     n = om_insert(ds, base);
-    //    RDTOOL_INTERVAL_END(OM_INSERT);
+    assert(n); /// @todo sequentially, this should always be true. In parallel, not.
   }
-  //  RDTOOL_INTERVAL_BEGIN(HEAVY_ARRAY_MANIP);
   if (!n->list->half_full
       && count_set_bits(n->label) >= HALF_BITS) {
     n->list->half_full = 1;
-    //    printf("List marked as half full with %zu items.\n", n->list->size);
+    RD_DEBUG("List marked as half full with %zu items.\n", n->list->size);
     heavy_nodes.push();
     *heavy_nodes.head() = om_get_tl(n);
   }
-  //RDTOOL_INTERVAL_END(HEAVY_ARRAY_MANIP);
-
-  //  RDTOOL_INTERVAL_END(TOOL_INSERT);
   return n;
 }
 
 extern "C" void cilk_tool_init(void) 
 {
-  //  RDTOOL_INTERVAL_BEGIN(TOOL);
 #ifdef STATS
   EMPTY_TIME_POINT.tv_sec = 0; EMPTY_TIME_POINT.tv_nsec = 0;
   for (int i = 0; i < NUM_INTERVAL_TYPES; ++i) {
@@ -157,15 +141,8 @@ extern "C" void cilk_tool_init(void)
 #endif
   g_english = om_new();
   g_hebrew = om_new();
-
-  //  RDTOOL_INTERVAL_BEGIN(SHADOW_STACK_MANIP);
   frames = new Stack_t<FrameData_t>[__cilkrts_get_nworkers()];
-  //  RDTOOL_INTERVAL_END(SHADOW_STACK_MANIP);
-
   g_tool_init = 1;
-
-  /// @todo allocate worker locks
-  //  RDTOOL_INTERVAL_END(TOOL);
 }
 
 extern "C" void cilk_tool_print(void)
@@ -180,28 +157,17 @@ extern "C" void cilk_tool_print(void)
 #endif
   std::cout << "Num relabels: " << g_num_relabels << std::endl;
   std::cout << "Num inserts: " << g_num_inserts << std::endl;
-  //  om_print(g_english);
 }
 
 extern "C" void cilk_tool_destroy(void) 
 {
-  //  RDTOOL_INTERVAL_BEGIN(TOOL);
-  cilk_tool_print();
+  //cilk_tool_print();
   om_free(g_english);
   om_free(g_hebrew);
-  //  RDTOOL_INTERVAL_BEGIN(SHADOW_STACK_MANIP);
   delete[] frames;
-  //  RDTOOL_INTERVAL_END(SHADOW_STACK_MANIP);
-  //  RDTOOL_INTERVAL_END(TOOL);
 }
 
-extern "C" void cilk_tool_c_function_enter(void* this_fn, void* rip)
-{
-  //  RDTOOL_INTERVAL_BEGIN(TOOL);
-  int id = __cilkrts_get_worker_number();
-  if (!frames[id].empty()) assert(frames[id].head()->type == USER);
-  //  RDTOOL_INTERVAL_END(TOOL);
-}
+extern "C" void cilk_tool_c_function_enter(void* this_fn, void* rip){ }
 extern "C" void cilk_tool_c_function_leave(void* rip) { }
 
 extern "C" void cilk_enter_begin(__cilkrts_stack_frame* sf, void* this_fn, void* rip){ }
@@ -210,26 +176,26 @@ extern "C" void cilk_enter_helper_begin(__cilkrts_stack_frame* sf, void* this_fn
 {
   __cilkrts_worker* w = __cilkrts_get_tls_worker();
   if (__cilkrts_get_batch_id(w) != -1) return;
-  //  RDTOOL_INTERVAL_BEGIN(TOOL);
+  if (frames[w->self].empty()) {
+    init_strand(w->self); // @change
+  }
   assert(!frames[w->self].empty());
 
   FrameData_t* parent = frames[w->self].head(); // helper frame parent
-  assert(parent->type == USER);
-  //  RDTOOL_INTERVAL_BEGIN(SHADOW_STACK_MANIP);
   frames[w->self].push();
-  frames[w->self].head()->type = HELPER;
-  frames[w->self].push();
-  //  RDTOOL_INTERVAL_END(SHADOW_STACK_MANIP);
   FrameData_t* f = frames[w->self].head();
-
-  f->type = USER;
+  f->enter_counter = 0;
+  RD_DEBUG("helper_begin: enter_counter = 0 -- ");
 
   if (!parent->sync_english) { // first of spawn group
     assert(!parent->sync_hebrew);
+    RD_DEBUG("first of spawn group\n");
     parent->sync_english =
       insert_or_relabel(w, g_english, heavy_english, parent->current_english);
     parent->sync_hebrew =
       insert_or_relabel(w, g_hebrew, heavy_hebrew, parent->current_hebrew);
+  } else {
+    RD_DEBUG("NOT first of spawn group\n");
   }
 
   f->current_english =
@@ -242,72 +208,80 @@ extern "C" void cilk_enter_helper_begin(__cilkrts_stack_frame* sf, void* this_fn
   f->current_hebrew =
     insert_or_relabel(w, g_hebrew, heavy_hebrew, parent->cont_hebrew);
   
-  f->cont_english = NULL; f->cont_hebrew = NULL;
-  f->sync_english = NULL; f->sync_hebrew = NULL;
-  parent->current_english = NULL; parent->current_hebrew = NULL;
-  //  RDTOOL_INTERVAL_END(TOOL);
+  f->cont_english = NULL;
+  f->cont_hebrew = NULL;
+  f->sync_english = NULL; 
+  f->sync_hebrew = NULL;
+  parent->current_english = NULL; 
+  parent->current_hebrew = NULL;
 }
 
 extern "C" void cilk_enter_end (__cilkrts_stack_frame* sf, void* rsp)
 {
   if (__cilkrts_get_batch_id(sf->worker) != -1) return;
-  //  RDTOOL_INTERVAL_BEGIN(TOOL);
+  if (!g_tool_init) {
+    printf("cilk_enter_end without tool init.\n");
+    return;
+  }
+  if (!frames[sf->worker->self].empty()) {
+    FrameData_t* f = frames[sf->worker->self].head();
+    f->enter_counter++;
+    RD_DEBUG("cilk_enter_end: incrementing enter_counter to %zu\n", f->enter_counter);
+  }
 
-  if (frames[sf->worker->self].empty()) init_strand(sf->worker->self);
-  else assert(frames[sf->worker->self].head()->type == USER);
-
-  //  RDTOOL_INTERVAL_END(TOOL);
+  /// @todo init_strand here, like before
+  //  if (frames[sf->worker->self].empty()) init_strand(sf->worker->self); // @change
 }
 
 extern "C" void cilk_spawn_prepare (__cilkrts_stack_frame* sf) { }
-
-extern "C" void cilk_spawn_or_continue (int in_continuation)
-{
-  //  RDTOOL_INTERVAL_BEGIN(TOOL);
-  assert(!in_continuation);
-  //  RDTOOL_INTERVAL_END(TOOL);
-}
-
+extern "C" void cilk_spawn_or_continue (int in_continuation){ }
 extern "C" void cilk_detach_begin (__cilkrts_stack_frame* parent) { }
-
 extern "C" void cilk_detach_end (void) { }
 
-extern "C" void cilk_sync_begin (__cilkrts_stack_frame* sf) { }
-
-extern "C" void cilk_sync_end (__cilkrts_stack_frame* sf)
-{
+extern "C" void cilk_sync_begin (__cilkrts_stack_frame* sf)
+{ 
   if (__cilkrts_get_batch_id(sf->worker) != -1) return;
-  //  RDTOOL_INTERVAL_BEGIN(TOOL);
+  if (frames[sf->worker->self].empty()) return;
   FrameData_t* f = frames[sf->worker->self].head();
-  assert(f->current_english); assert(f->current_hebrew);
-  assert(!f->cont_english); assert(!f->cont_hebrew);
+  RD_DEBUG("cilk_sync_begin with g_enter_counter == %zu\n", f->enter_counter);
+  if (f->enter_counter != 0) return;
 
-  if (f->sync_english) { // spawned
-    assert(f->sync_hebrew);
-    f->current_english = f->sync_english;
-    f->current_hebrew = f->sync_hebrew;
-    f->sync_english = NULL; f->sync_hebrew = NULL;
+  assert(f->current_english); 
+  assert(f->current_hebrew);
+  assert(!f->cont_english); 
+  assert(!f->cont_hebrew);
+
+   if (f->sync_english) { // spawned
+       assert(f->sync_hebrew);
+       RD_DEBUG("function spawned (in cilk_sync_begin)\n");
+       f->current_english = f->sync_english;
+       f->current_hebrew = f->sync_hebrew;
+       f->sync_english = NULL; 
+       f->sync_hebrew = NULL;
   } else { // function didn't spawn
-    assert(!f->sync_english); assert(!f->sync_hebrew);
+    assert(!f->sync_english); 
+    assert(!f->sync_hebrew);
   }
-
-  //  RDTOOL_INTERVAL_END(TOOL);
 }
+
+extern "C" void cilk_sync_end (__cilkrts_stack_frame* sf){ }
 
 extern "C" void cilk_leave_begin (__cilkrts_stack_frame *sf)
 {
-  //  RDTOOL_INTERVAL_BEGIN(TOOL);
-  if (__cilkrts_get_batch_id(sf->worker) != -1) goto exit;
+  if (__cilkrts_get_batch_id(sf->worker) != -1) return;
 
-  if (frames[sf->worker->self].size() > 1) {
-    frame_t type = frames[sf->worker->self].head()->type;
+  RD_DEBUG("cilk_leave_begin\n");
+  if (frames[sf->worker->self].size() <= 1) return;
 
-    //    RDTOOL_INTERVAL_BEGIN(SHADOW_STACK_MANIP);
+  FrameData_t* current = frames[sf->worker->self].head();
+  current->enter_counter--;
+  RD_DEBUG("cilk_leave_begin, decrementing g_enter_counter to %zu\n", current->enter_counter);
+
+  if (current->enter_counter == 0) {
+    RD_DEBUG("cilk_leave_begin: popping and changing nodes.\n");
+
     frames[sf->worker->self].pop();
-    //    RDTOOL_INTERVAL_END(SHADOW_STACK_MANIP);
-
-    if (type == HELPER) goto exit;
-    FrameData_t* current = frames[sf->worker->self].ancestor(1);
+    current = frames[sf->worker->self].head();
     assert(!current->current_english);
     assert(!current->current_hebrew);
     assert(current->cont_english);
@@ -319,9 +293,6 @@ extern "C" void cilk_leave_begin (__cilkrts_stack_frame *sf)
     current->cont_english = NULL;
     current->cont_hebrew = NULL;
   }
- exit:
-  ;
-  //  RDTOOL_INTERVAL_END(TOOL);
 }
 
 extern "C" void cilk_leave_end (void){ }
