448d3b17d90a0d8d813466ed09314d08dce9029c
Robert Utterback <robertu13@gmail.com>
2015-10-14 17:25:26 -0500

    Added extra function that the user can call: disabled all instrumention for all threads at a particular point.

--- rdtool/omrd.h
+++ rdtool/omrd.h
@@ -4,5 +4,6 @@
 extern "C" void cilk_tool_print(void);
 extern "C" void cilk_tool_destroy(void);
 extern "C" void __om_enable_checking(); 
-extern "C" void __om_disable_checking(); 
+extern "C" void __om_disable_checking();
+extern "C" void __om_disable_instrumentation();
 #endif // _OMRD_H
