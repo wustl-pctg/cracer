7c0587d559727e7500a391d3c5b9d02b00b7fe77
Robert Utterback <robertu13@gmail.com>
2015-11-18 10:01:23 -0600

    moving basic tests

---
+++ rdtool/test/basic/test8.cpp
@@ -0,0 +1,28 @@
+// expect a race on heap-allocated data
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+
+//#include "cilksan.h"
+#include "../../print_addr.h"
+
+void foo(char *x) {
+  x[0] = 1;
+}
+
+int main() {
+  {
+    char *x = (char*)malloc(16);
+    printf("%p\n", x);
+    _Cilk_spawn foo(x);
+    x[0] = 0;
+    _Cilk_sync;
+    free(x);
+  }
+  //  assert(__cilksan_error_count() == 1);
+  int num_races = get_num_races_found(); 
+  assert(num_races == 1);
+
+    
+  return 0;
+}
