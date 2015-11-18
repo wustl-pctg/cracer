59fa0928ea5a665bbbba6dfa8f96b7d190792410
Robert Utterback <robertu13@gmail.com>
2015-11-18 09:53:56 -0600

    Moving files

--- rdtool/src/debug_util.h
+++ rdtool/src/debug_util.h
@@ -5,6 +5,8 @@
 
 #ifndef DNDEBUG
 #define OM_DEBUG 1
+#else
+#define OM_DEBUG 0
 #endif
 
 // debug_level is a bitmap
@@ -17,31 +19,36 @@ enum debug_levels {
     DEBUG_MEMORY     = 8
 };
 
+__attribute__((noreturn)) 
+void die(const char *fmt, ...);
+void debug_printf(int level, const char *fmt, ...);
 
-#if OM_DEBUG
+#if OM_DEBUG > 0
 static int debug_level = DEBUG_CALLBACK | DEBUG_MEMORY;
 //static int debug_level = 0;
 #else
 static int debug_level = 0;
 #endif
 
-#if OM_DEBUG
-#define WHEN_OM_DEBUG(stmt) do { stmt } while(0);
-#define om_assert(c) \
-    do { if (!(c)) { die("%s:%d assertion failure: %s\n", \
-                        __FILE__, __LINE__, #c);} } while (0) 
-#define locked_assert(c) \
-  do { pthread_spin_lock(&g_worker_mutexes[self]); \
-  assert(c); \
-  pthread_spin_unlock(&g_worker_mutexes[self]); \
-  } while (0)
-#else 
+/* #if OM_DEBUG > 0 */
+/* #define WHEN_OM_DEBUG(stmt) do { stmt } while(0); */
+/* #define om_assert(c) \ */
+/*     do { if (!(c)) { die("%s:%d assertion failure: %s\n", \ */
+/*                         __FILE__, __LINE__, #c);} } while (0) */
+/* #define locked_assert(c) */
+/* // A deadlock involved getting the worker deque lock in batchify */
+/* // prevents using this */
+/*   /\* do { pthread_spin_lock(&g_worker_mutexes[self]); \ *\/ */
+/*   /\* assert(c); \ *\/ */
+/*   /\* pthread_spin_unlock(&g_worker_mutexes[self]); \ *\/ */
+/*   /\* } while (0) *\/ */
+/* #else */
 #define WHEN_OM_DEBUG(stmt)
 #define om_assert(c)
 #define locked_assert(c)
-#endif
+/* #endif */
 
-#if OM_DEBUG
+#if OM_DEBUG > 0
 // debugging assert to check that the tool is catching all the runtime events
 // that are supposed to match up (i.e., has event begin and event end)
 enum EventType_t { ENTER_FRAME = 1, ENTER_HELPER = 2, SPAWN_PREPARE = 3,
@@ -49,10 +56,6 @@ enum EventType_t { ENTER_FRAME = 1, ENTER_HELPER = 2, SPAWN_PREPARE = 3,
                    RUNTIME_LOOP = 7, NONE = 8 };
 #endif
 
-
-__attribute__((noreturn)) 
-void die(const char *fmt, ...);
-void debug_printf(int level, const char *fmt, ...);
 
 
 /// @todo temporarily disabled...why did I do this?
