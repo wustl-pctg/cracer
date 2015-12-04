12adf88951989db64807031308c6da565fc02e62
Robert Utterback <robertu13@gmail.com>
2015-12-04 14:07:38 -0600

    Forgot that the personal config file should not be included in git

--- rdtool/config.mk
+++
@@ -1,5 +0,0 @@
-COMPILER_HOME ?= /home/rob/llvm-cilk
-#TOOL_HOME ?= /home/rob/devel/cilksan
-RUNTIME_INTERNAL ?= /home/rob/devel/batch/cilkplusrts
-RUNTIME_LIB ?= /home/rob/llvm-cilk/lib/x86_64/libcilkrts.a
-MALLOC=-ltcmalloc # Can also be empty or "-ltbbmalloc_proxy"
