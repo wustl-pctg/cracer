59ac63b652789d415528fbd13f672d80cfda8461
Robert Utterback <robertu13@gmail.com>
2015-12-07 20:06:31 -0600

    Header file with preprocessor definitions to make it easier to switch benchmark versions

---
+++ rdtool/test/bench/bench.h
@@ -0,0 +1,20 @@
+#ifndef _BENCH_H
+#define _BENCH_H
+
+// This is a hack. I'm sharing the rd.h file between tests and the
+// actual library source, so this is a separate file. Ideally the two
+// should be split, and this file should be merged with one in
+// $(BASE_DIR)/include.
+
+#ifdef RACEDETECT
+#define RD_ENABLE __om_enable_checking()
+#define RD_DISABLE __om_disable_checking()
+
+#include "rd.h"
+#include "../src/print_addr.h"
+#else
+#define RD_ENABLE
+#define RD_DISABLE
+#endif
+
+#endif
