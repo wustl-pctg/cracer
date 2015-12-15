06908d58139b45f42668d302a05c9fa101b164b3
Robert Utterback <robertu13@gmail.com>
2015-12-14 21:53:39 -0600

    Made the benchmark script significantly nicer, including printing a status indicating the time each benchmark was started. It overwrites this line each, and surprising, it works just fine if you pipe it to a log file.

--- rdtool/test/bench/run_bench.py
+++ rdtool/test/bench/run_bench.py
@@ -5,64 +5,80 @@ import os, sys, subprocess, shlex
 import StringIO
 
 cilkscreen = "/home/rob/src/cilktools-linux-004421/bin/cilkscreen"
+use_cilkscreen = True
 
-## @todo{Use `git rev-parse HEAD` to get the commit id, which should
-## be output to the logs. That way I can save every log file and know
-## exactly which code was used to run it}
+def runit(prog, args, env):
+    cmd = prog + " " + args
+    proc = subprocess.Popen(cmd.split(), shell=False, env=env,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)
+    out, err = proc.communicate()
+    buf = StringIO.StringIO(out)
+    result = buf.readline()[:-1]
+    return result
 
-def run_tests():
-    cores = [4,6,8]# + (range(2,17,2))
-    tests = ["cholesky"]#["matmul", "fft", "cholesky"]
-    args = ["-n 1000 -z 4000"]#["-n 1024", "-n 1048576", "-n 1000 -z 4000"]
-    #args = ["-n 32", "-n 32", "-n 100 -z 20"]
-    comp = ["base", "insert", "bench", "icc"]
-    use_cilkscreen = True
+def pre_status(prev_line):
+    start_time = subprocess.check_output("date".split(), shell=False)
+    s = "Started latest at {}".format(start_time)[0:-1]
+    s = s.rjust(160 - len(prev_line))
+    print("{}".format(s), end='')
+    sys.stdout.flush()    
 
+def post_status(new_line):
+    print("\r{}".format(new_line), end='')
+    sys.stdout.flush()
+
+def print_header(comp):
+    commit = subprocess.check_output("git rev-parse HEAD".split(), shell=False)[0:-1]
+    print("Running benchmarks with tool built from {}".format(commit))
     print("{},".format('P'), end='')
-    for c in comp:
-        print("{: >20},".format(c), end='')
     if use_cilkscreen:
         print("{: >20},".format("cilkscreen"), end='')
+    for c in comp:
+        print("{: >20},".format(c), end='')
     print()
+
+
+def run_tests():
+    cores = [1] + range(2,17,2)
+    tests = ["matmul", "fft", "cholesky"]
+    args = ["-n 1024", "-n 1048576", "-n 1000 -z 4000"]
+    #args = ["-n 32", "-n 32", "-n 100 -z 20"]
+    comp = ["base", "insert", "bench", "icc"]
+
+    print_header(comp)
 
     for i in range(0,len(tests)):
         base = tests[i]
         print("------------------------------")
         print("Running {}:".format(base))
         base = os.path.join(os.getcwd(), base)
-
-        if use_cilkscreen:
-            prog = cilkscreen + " -- " + base + "_icc"
-            cmd = prog + " " + args[i]
-            proc = subprocess.Popen(cmd.split(), shell=False,
-                                    stdout=subprocess.PIPE,
-                                    stderr=subprocess.PIPE)
-            out, err = proc.communicate()
-            buf = StringIO.StringIO(out)
-            cilkscreen_result = buf.readline()[:-1]
+        cilkscreen_result = ""
 
         for p in cores:
-            print("{},".format(p), end='')
-            sys.stdout.flush()
             env = dict(os.environ, CILK_NWORKERS=str(p))
+            line = "{:2},".format(p)
+            print(line, end='')
+
+            if use_cilkscreen:
+                if p == 1:
+                    prog = cilkscreen + " -- " + base + "_icc"
+                    pre_status(line)
+                    cilkscreen_result = runit(prog, args[i], env)
+                line += "{: >20},".format(cilkscreen_result)
+                post_status(line)
 
             for c in comp:
                 prog = base + "_" + c
-                cmd = prog + " " + args[i]
+                pre_status(line)
+                result = runit(prog, args[i], env)
+                line += "{: >20},".format(result)
+                post_status(line)
 
-                proc = subprocess.Popen(cmd.split(), env=env, shell=False,
-                                        stdout=subprocess.PIPE,
-                                        stderr=subprocess.PIPE)
-                out, err = proc.communicate()
-                buf = StringIO.StringIO(out)
-                print("{: >20},".format(buf.readline()[:-1]), end='')
-                sys.stdout.flush()
-
-            if use_cilkscreen:
-                print("{: >20},".format(cilkscreen_result), end='')
-                sys.stdout.flush()
-
-            print()
+            s = " " * 20
+            s = s.rjust(160 - len(line))
+            print(s)
+            sys.stdout.flush()    
 
 def main(argv=None):
     run_tests()
