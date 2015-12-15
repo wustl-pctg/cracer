d93636a150a8951b7469d6db9da53bf9335ffa99
Robert Utterback <robertu13@gmail.com>
2015-12-15 17:20:59 -0600

    Benchmark script improvements

--- rdtool/test/bench/run_bench.py
+++ rdtool/test/bench/run_bench.py
@@ -6,43 +6,54 @@ import StringIO
 
 cilkscreen = "/home/rob/src/cilktools-linux-004421/bin/cilkscreen"
 use_cilkscreen = True
+print_status = True # Makes log file ugly
+column_format = "{: >25},"
+status_column = 200
 
-def runit(prog, args, env):
+def runit(n, prog, args, env):
     cmd = prog + " " + args
-    proc = subprocess.Popen(cmd.split(), shell=False, env=env,
-                            stdout=subprocess.PIPE,
-                            stderr=subprocess.PIPE)
-    out, err = proc.communicate()
-    buf = StringIO.StringIO(out)
-    result = buf.readline()[:-1]
-    return result
+    results = []
+    for i in range(n):
+        proc = subprocess.Popen(cmd.split(), shell=False, env=env,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+        out, err = proc.communicate()
+        buf = StringIO.StringIO(out)
+        results += [int(buf.readline()[:-1])]
+    avg = sum(results) / float(n)
+    return "{0:.2f}".format(avg)
 
 def pre_status(prev_line):
+    if not print_status: return
     start_time = subprocess.check_output("date".split(), shell=False)
     s = "Started latest at {}".format(start_time)[0:-1]
-    s = s.rjust(160 - len(prev_line))
-    print("{}".format(s), end='')
+    s = s.rjust(status_column - len(prev_line))
+    print(s, end='')
     sys.stdout.flush()    
 
 def post_status(new_line):
-    print("\r{}".format(new_line), end='')
+    if print_status:
+        new_line = '\r' + new_line
+    print(new_line, end='')
     sys.stdout.flush()
 
 def print_header(comp):
     commit = subprocess.check_output("git rev-parse HEAD".split(), shell=False)[0:-1]
     print("Running benchmarks with tool built from {}".format(commit))
-    print("{},".format('P'), end='')
+    print("P", end='')
     if use_cilkscreen:
-        print("{: >20},".format("cilkscreen"), end='')
+        print(column_format.format("cilkscreen"), end='')
     for c in comp:
-        print("{: >20},".format(c), end='')
+        print(column_format.format(c), end='')
     print()
 
 
 def run_tests():
+    num_iter = 3
     cores = [1] + range(2,17,2)
     tests = ["matmul", "fft", "cholesky"]
     args = ["-n 1024", "-n 1048576", "-n 1000 -z 4000"]
+    #args = ["-n 4096", "-n 2097152", "-n 2000 -z 8000"]
     #args = ["-n 32", "-n 32", "-n 100 -z 20"]
     comp = ["base", "insert", "bench", "icc"]
 
@@ -51,7 +62,7 @@ def run_tests():
     for i in range(0,len(tests)):
         base = tests[i]
         print("------------------------------")
-        print("Running {}:".format(base))
+        print("Running {} {} times with '{}':".format(base,num_iter,args[i]))
         base = os.path.join(os.getcwd(), base)
         cilkscreen_result = ""
 
@@ -64,19 +75,25 @@ def run_tests():
                 if p == 1:
                     prog = cilkscreen + " -- " + base + "_icc"
                     pre_status(line)
-                    cilkscreen_result = runit(prog, args[i], env)
-                line += "{: >20},".format(cilkscreen_result)
+                    cilkscreen_result = runit(num_iter, prog, args[i], env)
+                if not print_status: line = ""
+                line += column_format.format(cilkscreen_result)
                 post_status(line)
 
             for c in comp:
                 prog = base + "_" + c
                 pre_status(line)
-                result = runit(prog, args[i], env)
-                line += "{: >20},".format(result)
+                result = runit(num_iter, prog, args[i], env)
+                if not print_status: line = ""
+                line += column_format.format(result)
                 post_status(line)
 
-            s = " " * 20
-            s = s.rjust(160 - len(line))
+            if print_status:
+                s = " " * 20
+                s = s.rjust(status_column - len(line))
+                #post_status("\r\r\r" + line)
+            else:
+                s = ""
             print(s)
             sys.stdout.flush()    
 
