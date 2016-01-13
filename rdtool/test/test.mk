bf60d9b84f570887dd6189852d24f316bcb0c827
robertutterback <robert.utterback@wustl.edu>
2016-01-13 10:48:56 -0600

    adding insert-only flags

--- rdtool/test/test.mk
+++ rdtool/test/test.mk
@@ -6,6 +6,7 @@ TEST_MK_DIR:=$(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))
 include $(TEST_MK_DIR)/../common.mk
 
 RDFLAGS = -DRACEDETECT -fcilktool -fsanitize=thread
+INSERTFLAGS = -DINSERTSONLY -fcilktool
 LDFLAGS += -ldl -lpthread $(MALLOC)
 CFLAGS += 
 CXXFLAGS += 
