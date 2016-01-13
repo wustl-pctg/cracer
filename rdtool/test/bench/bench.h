315d5879543fd29366d2ef3652be815704ecfa8b
Robert Utterback <robertu13@gmail.com>
2016-01-13 11:29:34 -0600

    A better benchmark header that automatically adds program constructor and destructor to initialize/destroy the tool, disable the tool when necessary (inserts-only), and print out results

--- rdtool/test/bench/bench.h
+++ rdtool/test/bench/bench.h
@@ -9,7 +9,7 @@
 // should be split, and this file should be merged with one in
 // $(BASE_DIR)/include.
 
-#ifdef RACEDETECT
+#if defined RACEDETECT || defined INSERTSONLY
 #define RD_ENABLE __om_enable_checking()
 #define RD_DISABLE __om_disable_checking()
 
@@ -20,9 +20,36 @@
 #define RD_DISABLE
 #endif
 
+#ifdef INSERTSONLY
+extern "C" void __tsan_init();
+void init_inserts_only(void) __attribute__((constructor));
+void init_inserts_only(void)
+{
+  __tsan_init();
+  RD_DISABLE;
+  __om_disable_instrumentation();
+}
+
+void destroy_inserts_only(void) __attribute__((destructor));
+void destroy_inserts_only(void)
+{
+  cilk_tool_destroy();
+}
+#endif
+
+#ifdef RACEDETECT
+void destroy_rd(void) __attribute__((destructor));
+void destroy_rd(void)
+{
+  assert(get_num_races_found() == 0);
+  cilk_tool_destroy();
+}
+#endif
+
 #ifdef STATS
 #include "stattool.h"
-void __stattool_print_info() {} // @todo fix this hack
+void print_final_stats(void) __attribute__((destructor));
+void print_final_stats(void) {}
 #endif
 
 #endif
