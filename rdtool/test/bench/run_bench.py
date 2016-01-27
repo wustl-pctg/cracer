5d0c5475d20c3622539ca0c39908395d7dbed584
Robert Utterback <robertu13@gmail.com>
2016-01-26 23:36:05 -0600

    Cleaned up the run_bench script

--- rdtool/test/bench/run_bench.py
+++ rdtool/test/bench/run_bench.py
@@ -10,7 +10,7 @@ import StringIO, datetime, argparse
 ## python: import datetime; datetime.datetime.now().strftime("%Y%m%d-%H%M")
 
 cilkscreen = "/home/rob/src/cilktools-linux-004421/bin/cilkscreen"
-use_cilkscreen = False
+use_cilkscreen = True
 print_status = True # Makes log file ugly
 column_size = 22
 column_format = "{: >" + str(column_size) + "},"
@@ -58,6 +58,8 @@ def post_status(new_line):
     if print_status:
         sys.stderr.write('\r')
         sys.stderr.flush()
+        sys.stdout.write('\r')
+        sys.stdout.flush()
     print(new_line, end='')
     sys.stdout.flush()
 
@@ -77,51 +79,44 @@ def run_tests():
     cores = [1] + range(2,17,2)
 
     runs = OrderedDict()
-    # Currently, knapsack is too short and lu segfaults on 4 procs
-    # runs = {"matmul": "-n 1024",
-    #         "fft": "-n " + str(1024),
-    #         "cholesky": "-n 500 -z 5000",
-    #         "cilksort": "-n 100000",
-    #         "strassen": "-n 512",
-    #         "fib": "20",
-    #         "fibx": "20",
-    #         "heat": "-nx 256 -ny 256 -nt 25",
-    #         "nqueens": "8",
-    #         "qsort": "100000",
-    #         "rectmul": "-x 256 -y 256 -z 256",
-    #          "knapsack": "-benchmark long",
-    #         # "lu": "-n 32",
-    # }
-    #runs["matmul"] = "-n 2048"
-    #runs["cilksort"] = "-n 25000000"
-    runs["qsort"] = "32" #str(64*1024*1024)
-    # runs["heat"] = "-nx 2048 -ny 2048 -nt 500"
+    runs["matmul"] = "-n 2048"
+    runs["cilksort"] = "-n 25000000"
+    runs["heat"] = "-nx 2048 -ny 2048 -nt 500"
+    runs["fft"] = "-n " + str(64*1024*1024)
+    runs["cholesky"] = "-n 2000 -z 20000"
+    runs["fib"] = "38"
+    #runs["nqueens"] = "13" ## @bug Detects race!
+    #runs["qsort"] = "32" #str(64*1024*1024)
     # runs["knapsack"] = "-benchmark long"
-    # runs["fft"] = "-n " + str(64*1024*1024)
     # runs["rectmul"] = "-x 4096 -y 4096 -z 4096"
-    #runs["cholesky"] = "-n 3000 -z 30000", #bug? Infinite loop, probably when clearing shadow memory!
     #runs["strassen"] = "-n 4096"
-    #runs["fib"] = "40", # Takes too long..
     #runs["fibx"] = "772"
-    #runs["nqueens"] = "14"
-    #runs["lu"] = "-n ", # bug
+    #runs["lu"] = "-n "
     
     tests = runs.keys()
     args = runs.values()
-
-    #comp = ["icc", "base", "insert", "brd", "cilksan"]
-    comp = ["insert"]
+    comp = ["icc", "base", "insert", "brd", "cilksan"]
+    bin_dir = "bin"
     global status_column
     status_column = (len(comp)+1) * (column_size+3) + 25
     if use_cilkscreen: status_column += column_size
-    bin_dir = "bin"
 
+    # Let's validate the benchmarks so we don't waste time
+    for i in range(0, len(tests)):
+        base = os.path.join(os.getcwd(), bin_dir, tests[i])
+        for c in comp:
+            prog = base + "_" + c
+            if not os.path.isfile(prog):
+                print("Error: {} does not exist!".format(prog))
+                sys.exit(1)
+    
     print_header(comp)
 
     for i in range(0,len(tests)):
         base = tests[i]
         print("------------------------------")
         print("Running {} {} times with '{}':".format(base,num_iter,args[i]))
+        sys.stdout.flush()
         base = os.path.join(os.getcwd(), bin_dir, base)
         cilkscreen_result = ""
         cilksan_result = ""
@@ -129,28 +124,28 @@ def run_tests():
         for p in cores:
             env = dict(os.environ, CILK_NWORKERS=str(p))
             line = "{:2},".format(p)
-            #print(line, end='')
+            print(line, end='')
+            sys.stdout.flush()
 
             for c in comp:
                 prog = base + "_" + c
+                if not print_status: line = ""
+                pre_status(line)
                 if c == "cilksan" and p != 1:
-                    if not print_status: line = ""
                     line += column_format.format(cilksan_result)
                     post_status(line)
                     continue
-                pre_status(line)
                 result = runit(num_iter, c, prog, args[i], env)
                 if c == "cilksan" and p == 1: cilksan_result = result
-                if not print_status: line = ""
                 line += column_format.format(result)
                 post_status(line)
 
             if use_cilkscreen:
+                if not print_status: line = ""
+                pre_status(line)
                 if p == 1:
                     prog = cilkscreen + " -- " + base + "_icc"
-                    pre_status(line)
                     cilkscreen_result = runit(num_iter, "cilkscreen", prog, args[i], env)
-                if not print_status: line = ""
                 line += column_format.format(cilkscreen_result)
                 post_status(line)
 
