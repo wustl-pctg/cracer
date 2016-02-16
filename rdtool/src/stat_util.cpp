a9e5993be91ed672987ff81bb2b6ddfeb4da7e54
Robert Utterback <robertu13@gmail.com>
2016-02-16 11:50:39 -0600

    Changes to timing/stat code

--- rdtool/src/stat_util.cpp
+++ rdtool/src/stat_util.cpp
@@ -5,16 +5,32 @@ __thread __cilkrts_worker* t_worker = NULL;
 __thread int self = -1;
 
 #if STATS > 1
-struct timespec *g_timing_events;
-struct timespec *g_timing_event_starts;//[NUM_INTERVAL_TYPES];
-struct timespec *g_timing_event_ends;//[NUM_INTERVAL_TYPES];
-struct timespec EMPTY_TIME_POINT;
-int g_nproc = 0;
+
+// struct padded_time *g_timing_events;
+// struct padded_time *g_timing_event_starts;
+// struct padded_time *g_timing_event_ends;
+// struct timespec EMPTY_TIME_POINT = {0,0};
+struct padded_time *g_timing;
+const uint64_t EMPTY_TIME_POINT = 0;
+
+const char* g_interval_strings[NUM_INTERVAL_TYPES] = {
+  NULL,
+  // "Relabel time",
+  // "Insert time",
+  "Query time",
+};
 #endif
 
 #if STATS > 0
+// struct stats_s g_stats = {0};
 unsigned long g_num_relabels = 0;
-unsigned long g_num_empty_relabels = 0;
 unsigned long g_num_inserts = 0;
 unsigned long g_relabel_size = 0;
+
+unsigned long g_num_reads;
+unsigned long g_num_writes;
+unsigned long g_num_queries;
+
+size_t g_heavy_node_info[16384];
+
 #endif
